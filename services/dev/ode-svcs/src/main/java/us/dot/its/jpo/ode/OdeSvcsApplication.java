317101210bcdca6d5cfba1bf5c3dc859f355ea21
572682 <musavi_hamid@bah.com>
2016-11-10 18:08:47 -0500

    Added a simple prototype upload service. Go to http://ip:8080 to bring up the upload form.

--- services/dev/ode-svcs/src/main/java/us/dot/its/jpo/ode/OdeSvcsApplication.java
+++ services/dev/ode-svcs/src/main/java/us/dot/its/jpo/ode/OdeSvcsApplication.java
@@ -1,12 +1,27 @@
 package us.dot.its.jpo.ode;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import us.dot.its.jpo.ode.storage.StorageProperties;
+import us.dot.its.jpo.ode.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class OdeSvcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OdeSvcsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
 	}
 }
