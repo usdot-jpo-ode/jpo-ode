4858f32ef5776a2e7883c7192613727f94aae94d
Tony Chen <tonychen091@gmail.com>
2016-12-07 22:33:19 -0500

    addding basic framework for websocket subscriber

---
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/HelloMessage.java
@@ -0,0 +1,21 @@
+package us.dot.its.jpo.ode;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
