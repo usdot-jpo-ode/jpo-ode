7bcbf8b9bfc034d0baeff2168f9c91086dc55eed
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-19 13:59:34 -0500

    Added OssPositionalAccuracy test file and made fixes per failed tests

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPositionalAccuracy.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPositionalAccuracy.java
@@ -1,36 +1,45 @@
 package us.dot.its.jpo.ode.plugin.j2735.oss;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import us.dot.its.jpo.ode.j2735.dsrc.PositionalAccuracy;
 import us.dot.its.jpo.ode.plugin.j2735.J2735PositionalAccuracy;
 
 public class OssPositionalAccuracy {
-	public static J2735PositionalAccuracy genericPositionalAccuracy(
-			PositionalAccuracy positionalAccuracy) {
-		J2735PositionalAccuracy genericPositionalAccuracy = 
-				new J2735PositionalAccuracy();
+    public static J2735PositionalAccuracy genericPositionalAccuracy(PositionalAccuracy positionalAccuracy) {
 
-		if (positionalAccuracy.semiMajor.intValue() != 255) {
-			genericPositionalAccuracy.semiMajor = 
-					BigDecimal.valueOf(
-							positionalAccuracy.semiMajor.intValue() * 5, 2);
-		}
-		
-		if (positionalAccuracy.semiMinor.intValue() != 255) {
-			genericPositionalAccuracy.semiMinor = 
-					BigDecimal.valueOf(
-							positionalAccuracy.semiMinor.intValue() * 5, 2);
-		}
-		
-		if (positionalAccuracy.orientation.intValue() != 65535) {
-			genericPositionalAccuracy.orientation = 
-					BigDecimal.valueOf(
-							(double)positionalAccuracy.orientation.longValue() / 65535.0);
-		}
-		return genericPositionalAccuracy;
-		
-	}
+        if (positionalAccuracy.semiMajor.intValue() < 0 || positionalAccuracy.semiMajor.intValue() > 255) {
+            throw new IllegalArgumentException("SemiMajorAccuracy value out of bounds");
+        }
 
+        if (positionalAccuracy.semiMinor.intValue() < 0 || positionalAccuracy.semiMinor.intValue() > 255) {
+            throw new IllegalArgumentException("SemiMinorAccuracy value out of bounds");
+        }
+
+        if (positionalAccuracy.orientation.intValue() < 0 || positionalAccuracy.orientation.intValue() > 65535) {
+            throw new IllegalArgumentException("SemiMajorOrientation value out of bounds");
+        }
+
+        J2735PositionalAccuracy genericPositionalAccuracy = new J2735PositionalAccuracy();
+
+        if (positionalAccuracy.semiMajor.intValue() != 255) {
+            genericPositionalAccuracy.semiMajor = BigDecimal.valueOf(positionalAccuracy.semiMajor.intValue() * 5, 2);
+        }
+
+        if (positionalAccuracy.semiMinor.intValue() != 255) {
+            genericPositionalAccuracy.semiMinor = BigDecimal.valueOf(positionalAccuracy.semiMinor.intValue() * 5, 2);
+        }
+
+        if (positionalAccuracy.orientation.intValue() != 65535) {
+
+            genericPositionalAccuracy.orientation = BigDecimal
+                    .valueOf((double) (0.0054932479) * (double) (positionalAccuracy.orientation.longValue()))
+                    .setScale(10, RoundingMode.HALF_EVEN);
+
+        }
+        return genericPositionalAccuracy;
+
+    }
 
 }
