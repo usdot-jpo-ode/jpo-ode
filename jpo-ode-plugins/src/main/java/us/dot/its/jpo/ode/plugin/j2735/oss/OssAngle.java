2849c4f7213303c0b0101aa0e6910bedf2c492e4
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-22 13:17:23 -0500

    Added OssFullPositionVector tests and made fixes where tests indicated errors. Misc cleanup and fixes.

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssAngle.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssAngle.java
@@ -6,24 +6,23 @@ import us.dot.its.jpo.ode.j2735.dsrc.Angle;
 
 public class OssAngle {
 
-	public static BigDecimal genericAngle(Angle angle) {
-	    
-	    if (angle.intValue() < 0 || angle.intValue() > 28800) {
-	        throw new IllegalArgumentException("Angle value out of bounds");
-	    }
-		
-		BigDecimal result = null;
-		
-		if (angle.longValue() != 28800) {
-			result = longToDecimal(angle.longValue());
-		}
-		
-		return result;
-	}
+    public static BigDecimal genericAngle(Angle angle) {
 
-	public static BigDecimal longToDecimal(long longValue) {
-		return BigDecimal.valueOf(longValue * 125, 4);
-	}
+        if (angle.intValue() < 0 || angle.intValue() > 28800) {
+            throw new IllegalArgumentException("Angle value out of bounds");
+        }
 
-	
+        BigDecimal result = null;
+
+        if (angle.longValue() != 28800) {
+            result = longToDecimal(angle.longValue());
+        }
+
+        return result;
+    }
+
+    public static BigDecimal longToDecimal(long longValue) {
+        return BigDecimal.valueOf(longValue * 125, 4);
+    }
+
 }
