b7929b5590a239f879b76a58e451629be10e840f
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-28 11:49:02 -0500

    Removed failing blank test

--- jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssNode_XYTest.java
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssNode_XYTest.java
@@ -11,14 +11,16 @@ import org.junit.Test;
  * Verifies correct conversion from Node-XY-24b to compliant J2735Node_XY
  * 
  * -- Documentation --
- * 
+ * Data Frame: DF_Node_XY_24b
+Use: A 24-bit node type with offset values from the last point in X and Y.
+ASN.1 Representation:
+Node-XY-24b ::= SEQUENCE {
+x Offset-B12,
+y Offset-B12
+}
  *
  */
 public class OssNode_XYTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
 
 }
