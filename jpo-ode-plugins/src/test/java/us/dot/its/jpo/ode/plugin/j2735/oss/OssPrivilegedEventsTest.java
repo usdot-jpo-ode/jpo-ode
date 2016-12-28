58be8290ecceba66017eed009cc3861d6b6e9fb4
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-28 10:52:43 -0500

    Changed OssPrivilegedEvents to perform bit string to hashmap conversion itself, rather than relying on OssBitString. Added J2735PrivilegedEventFlags.java and J2735PrivilegedEvents.java, abstract data members to support OssPrivilegedEvents. This commit begins the deprecation process of OssBitString, classes will now be restructured to perform their own bitstring to hashmap conversions.

--- jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPrivilegedEventsTest.java
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPrivilegedEventsTest.java
@@ -2,6 +2,8 @@ package us.dot.its.jpo.ode.plugin.j2735.oss;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -10,12 +12,13 @@ import com.oss.asn1.BitString;
 import us.dot.its.jpo.ode.j2735.dsrc.PrivilegedEventFlags;
 import us.dot.its.jpo.ode.j2735.dsrc.PrivilegedEvents;
 import us.dot.its.jpo.ode.j2735.dsrc.SSPindex;
+import us.dot.its.jpo.ode.plugin.j2735.J2735PrivilegedEvents;
 
 /**
  * -- Summary --
  * JUnit test class for OssPrivilegedEvents
  * 
- * Verifies correct conversion from generic PrivilegedEvents to compliant-J2735PrivilegedEvents
+ * Verifies correct conversion from generic PrivilegedEvents to compliant-J2735PrivilegedEvents.
  * 
  * -- Documentation --
  * Data Frame: DF_PrivilegedEvents
@@ -55,33 +58,365 @@ import us.dot.its.jpo.ode.j2735.dsrc.SSPindex;
  *          -- or is within the roadway. Note that lights and sirens
  *          -- may not be evident during any given response call 
  *          -- Emergency and Non Emergency Lights related
- *          peEmergencyLightsActive (2),
- *          peEmergencySoundActive (3),
- *          peNonEmergencyLightsActive (4),
- *          peNonEmergencySoundActive (5)
- *          -- this list is likely to grow with further peer review
- *          } (SIZE (16))
+ *       peEmergencyLightsActive (2),
+ *       peEmergencySoundActive (3),
+ *       peNonEmergencyLightsActive (4),
+ *       peNonEmergencySoundActive (5)
+ *       -- this list is likely to grow with further peer review
+ *       } (SIZE (16))
  */
 public class OssPrivilegedEventsTest {
     
+    // SSPindex tests
     
-    @Ignore // Test case depends on complete implementation of OssBitString
+    /**
+     * Test that the minimum ssp index value 0 returns 0
+     */
     @Test
-    public void shouldCreatePriviledgedEvents() {
+    public void shouldReturnMinimumSSPindex() {
         
-        // Set up SSPIndex
-        Integer testInputSSPIndex = -1;
-        SSPindex testSSPIndex = new SSPindex(testInputSSPIndex);
+        Integer testInput = 0;
+        Integer expectedValue = 0;
         
-        // Set up PrivilegedEventFlag
-        Integer testPrivilegedEventsFlagBitStringInteger = 0b00;
-        BitString testPrivilegedEventFlagBitString = new BitString();
+        SSPindex testSSPindex = new SSPindex(testInput);
         
-        PrivilegedEventFlags testPrivilegedEventFlags = new PrivilegedEventFlags();
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
         
-        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPIndex, testPrivilegedEventFlags);
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
         
-        assertEquals(1, testEvents.sspRights.intValue());
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        assertEquals(expectedValue, actualEvents.sspRights);
+        
+    }
+    
+    /**
+     * Test that a corner case minimum ssp index value 1 returns 1
+     */
+    @Test
+    public void shouldReturnCornerCaseMinimumSSPindex() {
+
+        Integer testInput = 1;
+        Integer expectedValue = 1;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        assertEquals(expectedValue, actualEvents.sspRights);
+    }
+    
+    /**
+     * Test that a middle ssp index value 15 returns 15
+     */
+    @Test
+    public void shouldReturnMiddleSSPindex() {
+
+        Integer testInput = 15;
+        Integer expectedValue = 15;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        assertEquals(expectedValue, actualEvents.sspRights);
+    }
+    
+    /**
+     * Test that a corner case maximum ssp index value 30 returns 30
+     */
+    @Test
+    public void shouldReturnCornerCaseMaximumSSPindex() {
+
+        Integer testInput = 30;
+        Integer expectedValue = 30;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        assertEquals(expectedValue, actualEvents.sspRights);
+    }
+    
+    /**
+     * Test that the maximum ssp index value 31 returns 31
+     */
+    @Test
+    public void shouldReturnMaximumSSPindex() {
+
+        Integer testInput = 31;
+        Integer expectedValue = 31;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        assertEquals(expectedValue, actualEvents.sspRights);
+    }
+    
+    /**
+     * Test that an ssp index value (-1) below the lower bound (0) throws IllegalArgumentException
+     */
+    @Test
+    public void shouldThrowExceptionSSPindexBelowLowerBound() {
+
+        Integer testInput = -1;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        try {
+            J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+            fail("Expected IllegalArgumentException");
+        } catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+    }
+    
+    /**
+     * Test that an ssp index value (32) above the upper bound (31) throws IllegalArgumentException
+     */
+    @Test
+    public void shouldThrowExceptionSSPindexAboveUpperBound() {
+
+        Integer testInput = 32;
+        
+        SSPindex testSSPindex = new SSPindex(testInput);
+        
+        Integer testEventBitString = 0b000000;
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        try {
+            J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+            fail("Expected IllegalArgumentException");
+        } catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+    }
+    
+    // Event flags tests
+    
+    /**
+     * Test input bit string "000000" returns "false" for all values
+     */
+    @Test
+    public void shouldCreateAllOffPrivilegedEvents() {
+        
+        Integer testEventBitString = 0b000000;
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> entry : actualEvents.event.entrySet()) {
+            
+            assertFalse("Expected " + entry.getKey() + " to be false", entry.getValue());
+            
+        }
+        
+    }
+    
+    /**
+     * Test input bit string "111111" returns "true" for all values
+     */
+    @Test
+    public void shouldCreateAllOnPrivilegedEvents() {
+        
+        Integer testEventBitString = 0b111111;
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> entry : actualEvents.event.entrySet()) {
+            assertTrue("Expected " + entry.getKey() + " to be true", entry.getValue());
+        }
+        
+    }
+    
+    /**
+     * Test input bit string "000001" (bit index #0 active) returns "true" for "peUnavailable" only
+     */
+    @Test
+    public void shouldCreateUnavailablePrivilegedEvent() {
+        
+        Integer testEventBitString = 0b000001;
+        String elementTested = "peUnavailable";
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> curVal : actualEvents.event.entrySet()) {
+            if(curVal.getKey() == elementTested) {
+                assertTrue("Expected " + curVal.getKey() + " to be true", curVal.getValue());
+            } else {
+                assertFalse("Expected " + curVal.getKey() + " to be false", curVal.getValue());
+            }
+        }
+        
+    }
+    
+    /**
+     * Test input bit string "000010" (bit index #1 active) returns "true" for "peEmergencyResponse" only
+     */
+    @Test
+    public void shouldCreateEmergencyResponsePrivilegedEvent() {
+        
+        Integer testEventBitString = 0b000010;
+        String elementTested = "peEmergencyResponse";
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> curVal : actualEvents.event.entrySet()) {
+            if(curVal.getKey() == elementTested) {
+                assertTrue("Expected " + curVal.getKey() + " to be true", curVal.getValue());
+            } else {
+                assertFalse("Expected " + curVal.getKey() + " to be false", curVal.getValue());
+            }
+        }   
+        
+    }
+    
+    /**
+     * Test input bit string "010000" (bit index #4 active) returns "true" for "peNonEmergencyLightsActive" only
+     */
+    @Test
+    public void shouldCreateEmergencyLightsActivePrivilegedEvent() {
+        
+        Integer testEventBitString = 0b010000;
+        String elementTested = "peNonEmergencyLightsActive";
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> curVal : actualEvents.event.entrySet()) {
+            if(curVal.getKey() == elementTested) {
+                assertTrue("Expected " + curVal.getKey() + " to be true", curVal.getValue());
+            } else {
+                assertFalse("Expected " + curVal.getKey() + " to be false", curVal.getValue());
+            }
+        }  
+        
+    }
+    
+    /**
+     * Test input bit string "100000" (bit index #5 active) returns "true" for "peNonEmergencySoundActive" only
+     */
+    @Test
+    public void shouldCreateNonEmergencySoundActivePrivilegedEvent() {
+        
+        Integer testEventBitString = 0b100000;
+        String elementTested = "peNonEmergencySoundActive";
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> curVal : actualEvents.event.entrySet()) {
+            if(curVal.getKey() == elementTested) {
+                assertTrue("Expected " + curVal.getKey() + " to be true", curVal.getValue());
+            } else {
+                assertFalse("Expected " + curVal.getKey() + " to be false", curVal.getValue());
+            }
+        }
+        
+    }
+    
+    /**
+     * Test input bit string "100001" returns "true" for both "peNonEmergencySoundActive" and "peUnavailable"
+     * and false for all other values
+     */
+    @Test
+    public void shouldCreateTwoPrivilegedEvents() {
+        
+        Integer testEventBitString = 0b100001;
+        String elementTested1 = "peNonEmergencySoundActive";
+        String elementTested2 = "peUnavailable";
+        
+        SSPindex testSSPindex = new SSPindex(0);
+        
+        byte[] testEventsBytes = {testEventBitString.byteValue()};
+        
+        PrivilegedEventFlags testPrivilegedEventsFlags = new PrivilegedEventFlags(testEventsBytes);
+        PrivilegedEvents testEvents = new PrivilegedEvents(testSSPindex, testPrivilegedEventsFlags);
+        
+        J2735PrivilegedEvents actualEvents = OssPrivilegedEvents.genericPrivilegedEvents(testEvents);
+        
+        for (Map.Entry<String, Boolean> curVal : actualEvents.event.entrySet()) {
+            if(curVal.getKey() == elementTested1 || curVal.getKey() == elementTested2) {
+                assertTrue("Expected " + curVal.getKey() + " to be true", curVal.getValue());
+            } else {
+                assertFalse("Expected " + curVal.getKey() + " to be false", curVal.getValue());
+            }
+        }
     }
 
 }
