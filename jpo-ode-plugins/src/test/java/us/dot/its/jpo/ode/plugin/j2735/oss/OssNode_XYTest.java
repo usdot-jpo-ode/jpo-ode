bb4c523808ef8d58680b667f3a4983f3d1216e9d
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-28 11:43:50 -0500

    Updated readme and added skeleton Node_XYTest class

---
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssNode_XYTest.java
@@ -0,0 +1,24 @@
+package us.dot.its.jpo.ode.plugin.j2735.oss;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * -- Summary --
+ * JUnit test class for OssNode_XY
+ * 
+ * Verifies correct conversion from Node-XY-24b to compliant J2735Node_XY
+ * 
+ * -- Documentation --
+ * 
+ *
+ */
+public class OssNode_XYTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
