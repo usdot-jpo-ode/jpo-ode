a031d428ef044ac32759685a7c1487c2d7281ca4
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-29 13:52:08 -0500

    Implemented OssAntennaOffsetSet. Added test class OssAntennaOffsetSetTest. Added supporting methods to OssOffset and J2735AntennaOffsetSet.

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735AntennaOffsetSet.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735AntennaOffsetSet.java
@@ -1,8 +1,32 @@
 package us.dot.its.jpo.ode.plugin.j2735;
 
+import java.math.BigDecimal;
+
 import us.dot.its.jpo.ode.plugin.asn1.Asn1Object;
 
 public class J2735AntennaOffsetSet extends Asn1Object {
 	private static final long serialVersionUID = 1L;
+	
+	private BigDecimal antOffsetX;
+	private BigDecimal antOffsetY;
+	private BigDecimal antOffsetZ;
+    public BigDecimal getAntOffsetX() {
+        return antOffsetX;
+    }
+    public void setAntOffsetX(BigDecimal antOffsetX) {
+        this.antOffsetX = antOffsetX;
+    }
+    public BigDecimal getAntOffsetY() {
+        return antOffsetY;
+    }
+    public void setAntOffsetY(BigDecimal antOffsetY) {
+        this.antOffsetY = antOffsetY;
+    }
+    public BigDecimal getAntOffsetZ() {
+        return antOffsetZ;
+    }
+    public void setAntOffsetZ(BigDecimal antOffsetZ) {
+        this.antOffsetZ = antOffsetZ;
+    }
 
 }
