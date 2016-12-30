81fddb2d8bffc1616b7ac54ae36ceff1d5700ccf
Matthew Schwartz <schwartz_matthew2@bah.com>
2016-12-30 12:57:08 -0500

    Added OssPathPrediction unit tests and fixed class where tests indicated errors.

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPathPrediction.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssPathPrediction.java
@@ -7,12 +7,23 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735PathPrediction;
 
 public class OssPathPrediction {
 
-	public static J2735PathPrediction genericPathPrediction(PathPrediction pathPrediction) {
-		J2735PathPrediction pp = new J2735PathPrediction();
-		
-		pp.confidence = BigDecimal.valueOf(pathPrediction.confidence.longValue() * 5, 1);
-		pp.radiusOfCurve = BigDecimal.valueOf(pathPrediction.radiusOfCurve.longValue(), 1);
-		return pp ;
-	}
+    public static J2735PathPrediction genericPathPrediction(PathPrediction pathPrediction) {
+        J2735PathPrediction pp = new J2735PathPrediction();
+
+        if (pathPrediction.radiusOfCurve.longValue() >= 32767 
+                || pathPrediction.radiusOfCurve.longValue() < -32767) {
+            pp.radiusOfCurve = BigDecimal.ZERO.setScale(1);
+        } else {
+            pp.radiusOfCurve = BigDecimal.valueOf(pathPrediction.radiusOfCurve.longValue(), 1);
+        }
+        
+        if (pathPrediction.confidence.longValue() < 0 || pathPrediction.confidence.longValue() > 200) {
+            throw new IllegalArgumentException("Confidence value out of bounds [0..200]");
+        } else {
+            pp.confidence = BigDecimal.valueOf(pathPrediction.confidence.longValue() * 5, 1);
+        }
+
+        return pp;
+    }
 
 }
