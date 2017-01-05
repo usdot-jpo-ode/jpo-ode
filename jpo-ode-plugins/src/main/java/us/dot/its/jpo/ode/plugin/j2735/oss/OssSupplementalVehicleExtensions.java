b1fb8fe53640fc4525dce153db6b05a1269751d3
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-01-05 15:35:04 -0500

    ODE-117 Added unit tests for OssSupplementalVehicleExtensions

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssSupplementalVehicleExtensions.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssSupplementalVehicleExtensions.java
@@ -10,24 +10,44 @@ public class OssSupplementalVehicleExtensions {
 			SupplementalVehicleExtensions sve) {
 		J2735SupplementalVehicleExtensions suppVeh = new J2735SupplementalVehicleExtensions();
 		
-		suppVeh.classification = sve.classification.intValue();
-		suppVeh.classDetails = OssVehicleClassification.genericVehicleClassification(sve.classDetails);
-		suppVeh.vehicleData = OssVehicleData.genericVehicleData(sve.vehicleData);
-		suppVeh.weatherReport = OssWeatherReport.genericWeatherReport(sve.weatherReport);
-		suppVeh.weatherProbe = OssWeatherProbe.genericWeatherProbe(sve.weatherProbe);
-		suppVeh.obstacle = OssObstacleDetection.genericObstacleDetection(sve.obstacle);
-		suppVeh.status = OssDisabledVehicle.genericDisabledVehicle(sve.status);
-		suppVeh.speedProfile = OssSpeedProfile.genericSpeedProfile(sve.speedProfile);
-		suppVeh.theRTCM = OssRTCMPackage.genericRTCMPackage(sve.theRTCM);
-		
-		while (sve.regional.elements().hasMoreElements()) {
-			us.dot.its.jpo.ode.j2735.dsrc.SupplementalVehicleExtensions.Regional.Sequence_ element = 
-					(us.dot.its.jpo.ode.j2735.dsrc.SupplementalVehicleExtensions.Regional.Sequence_) sve
-					.regional.elements().nextElement();
-			suppVeh.regional.add(new J2735RegionalContent()
-					.setId(element.regionId.intValue())
-					.setValue(element.regExtValue.getEncodedValue())
-					);
+		// All elements of this class are optional
+		if (sve.hasClassification()) {
+		    suppVeh.classification = sve.classification.intValue();
+		}
+		if (sve.hasClassDetails()) {
+		    suppVeh.classDetails = OssVehicleClassification.genericVehicleClassification(sve.classDetails);
+		}
+		if (sve.hasVehicleData()) {
+		    suppVeh.vehicleData = OssVehicleData.genericVehicleData(sve.vehicleData);
+		}
+		if (sve.hasWeatherReport()) {
+		    suppVeh.weatherReport = OssWeatherReport.genericWeatherReport(sve.weatherReport);
+		}
+		if (sve.hasWeatherProbe()) {
+		    suppVeh.weatherProbe = OssWeatherProbe.genericWeatherProbe(sve.weatherProbe);
+		}
+		if (sve.hasObstacle()) {
+		    suppVeh.obstacle = OssObstacleDetection.genericObstacleDetection(sve.obstacle);
+		}
+		if (sve.hasStatus()) {
+		    suppVeh.status = OssDisabledVehicle.genericDisabledVehicle(sve.status);
+		}
+		if (sve.hasSpeedProfile()) {
+		    suppVeh.speedProfile = OssSpeedProfile.genericSpeedProfile(sve.speedProfile);
+		}
+		if (sve.hasTheRTCM()) {
+		    suppVeh.theRTCM = OssRTCMPackage.genericRTCMPackage(sve.theRTCM);
+		}
+		if (sve.hasRegional()) {
+   		while (sve.regional.elements().hasMoreElements()) {
+   			us.dot.its.jpo.ode.j2735.dsrc.SupplementalVehicleExtensions.Regional.Sequence_ element = 
+   					(us.dot.its.jpo.ode.j2735.dsrc.SupplementalVehicleExtensions.Regional.Sequence_) sve
+   					.regional.elements().nextElement();
+   			suppVeh.regional.add(new J2735RegionalContent()
+   					.setId(element.regionId.intValue())
+   					.setValue(element.regExtValue.getEncodedValue())
+   					);
+   		}
 		}
 		return suppVeh;
 	}
