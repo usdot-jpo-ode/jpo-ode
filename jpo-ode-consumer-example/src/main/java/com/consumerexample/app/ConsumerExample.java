31fa6180f5b787b94a2f482c0b9eb0cb3749506a
Tony Chen <tonychen091@gmail.com>
2017-01-11 23:15:48 -0500

    Updated spaces with tabs

--- jpo-ode-consumer-example/src/main/java/com/consumerexample/app/ConsumerExample.java
+++ jpo-ode-consumer-example/src/main/java/com/consumerexample/app/ConsumerExample.java
@@ -10,44 +10,44 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735Bsm;
 import us.dot.its.jpo.ode.util.SerializationUtils;
 
 public class ConsumerExample {
-    
-    public static void main( String[] args ){
-    	
-    	// Option parsing
-    	Options options = new Options();
-    	
-    	Option bootstrap_server = new Option("b", "bootstrap-server", true, "Endpoint ('ip:port')");
-    	bootstrap_server.setRequired(true);
-    	options.addOption(bootstrap_server);
+	
+	public static void main( String[] args ){
+		
+		// Option parsing
+		Options options = new Options();
+		
+		Option bootstrap_server = new Option("b", "bootstrap-server", true, "Endpoint ('ip:port')");
+		bootstrap_server.setRequired(true);
+		options.addOption(bootstrap_server);
 
-    	Option topic_option = new Option("t", "topic", true, "Topic Name");
-    	topic_option.setRequired(true);
-    	options.addOption(topic_option);
+		Option topic_option = new Option("t", "topic", true, "Topic Name");
+		topic_option.setRequired(true);
+		options.addOption(topic_option);
 
-    	Option group_option = new Option("g", "group", true, "Consumer Group");
-    	group_option.setRequired(true);
-    	options.addOption(group_option);
+		Option group_option = new Option("g", "group", true, "Consumer Group");
+		group_option.setRequired(true);
+		options.addOption(group_option);
 
-    	Option type_option = new Option("type", "type", true, "string|byte message type");
-    	type_option.setRequired(true);
-    	options.addOption(type_option);
+		Option type_option = new Option("type", "type", true, "string|byte message type");
+		type_option.setRequired(true);
+		options.addOption(type_option);
 
-    	CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd;
 
-         try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("Consumer Example", options);
+		 try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			formatter.printHelp("Consumer Example", options);
 
-            System.exit(1);
-            return;
-        }
+			System.exit(1);
+			return;
+		}
 
-        String endpoint = cmd.getOptionValue("bootstrap-server");
-        String topic = cmd.getOptionValue("topic");
+		String endpoint = cmd.getOptionValue("bootstrap-server");
+		String topic = cmd.getOptionValue("topic");
 		String group = cmd.getOptionValue("group");
 		String type = cmd.getOptionValue("type");
 
@@ -71,13 +71,13 @@ public class ConsumerExample {
 			byteArrayConsumer.subscribe(Arrays.asList(topic));
 			System.out.println("Subscribed to topic " + topic);
 			while (true) {
-			 	ConsumerRecords<String, byte[]> records = byteArrayConsumer.poll(100);
-			    for (ConsumerRecord<String, byte[]> record : records) {
-			    	// Serialize the record value
-			    	SerializationUtils<J2735Bsm> serializer = new SerializationUtils<J2735Bsm>();
-			    	J2735Bsm bsm =  serializer.deserialize(record.value());
-			    	System.out.print(bsm.toString()); 
-			    }
+				ConsumerRecords<String, byte[]> records = byteArrayConsumer.poll(100);
+				for (ConsumerRecord<String, byte[]> record : records) {
+					// Serialize the record value
+					SerializationUtils<J2735Bsm> serializer = new SerializationUtils<J2735Bsm>();
+					J2735Bsm bsm =  serializer.deserialize(record.value());
+					System.out.print(bsm.toString()); 
+				}
 			}
 		} else {
 			KafkaConsumer<String, String> stringConsumer = new KafkaConsumer<String, String>(props);
@@ -86,11 +86,11 @@ public class ConsumerExample {
 			System.out.println("Subscribed to topic " + topic);
 			while (true) {		
 				ConsumerRecords<String, String> records = stringConsumer.poll(100);
-			    for (ConsumerRecord<String, String> record : records) {
-			    	System.out.print(record.value()); 
-			    }
+				for (ConsumerRecord<String, String> record : records) {
+					System.out.print(record.value()); 
+				}
 			}
 		
 		}
-    }
+	}
 }
