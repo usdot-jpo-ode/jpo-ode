3cc4312f7f577009814bfa5c3c2d9d6dd1848ef6
mgarramo <GarramoneJr_Michael@bah.com>
2017-01-24 12:02:25 -0500

    Added jmx for runtime logging configuration

--- docker/start-ode.sh
+++ docker/start-ode.sh
@@ -15,4 +15,11 @@ echo starting ODE
 cd /home
 #mkdir -p /home/uploads
 #java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=y -jar /home/jpo-ode-svcs-0.0.1-SNAPSHOT.jar
-java -jar /home/jpo-ode-svcs-0.0.1-SNAPSHOT.jar
+java -Djava.rmi.server.hostname=192.168.99.100 \
+	-Dcom.sun.management.jmxremote.port=9090 \
+	-Dcom.sun.management.jmxremote.rmi.port=9090 \
+	-Dcom.sun.management.jmxremote \
+	-Dcom.sun.management.jmxremote.local.only=false \
+	-Dcom.sun.management.jmxremote.authenticate=false \
+	-Dcom.sun.management.jmxremote.ssl=false \
+	-Dlogback.configurationFile=/home/logback.xml -jar /home/jpo-ode-svcs-0.0.1-SNAPSHOT.jar
