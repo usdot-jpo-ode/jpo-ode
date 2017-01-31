9c61c2505097b59193918bd1dbc746c2a835bb9c
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-01-31 08:09:50 -0500

    Refactor

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PathHistory.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PathHistory.java
@@ -5,9 +5,34 @@ import java.util.List;
 import us.dot.its.jpo.ode.plugin.asn1.Asn1Object;
 
 public class J2735PathHistory extends Asn1Object {
-	private static final long serialVersionUID = 1L;
-	
-	public J2735FullPositionVector initialPosition;
-	public J2735BitString currGNSSstatus;
-	public List<J2735PathHistoryPoint> crumbData;
+    private static final long serialVersionUID = 1L;
+
+    private J2735FullPositionVector initialPosition;
+    private J2735BitString currGNSSstatus;
+    private List<J2735PathHistoryPoint> crumbData;
+
+    public J2735FullPositionVector getInitialPosition() {
+        return initialPosition;
+    }
+
+    public void setInitialPosition(J2735FullPositionVector initialPosition) {
+        this.initialPosition = initialPosition;
+    }
+
+    public J2735BitString getCurrGNSSstatus() {
+        return currGNSSstatus;
+    }
+
+    public void setCurrGNSSstatus(J2735BitString currGNSSstatus) {
+        this.currGNSSstatus = currGNSSstatus;
+    }
+
+    public List<J2735PathHistoryPoint> getCrumbData() {
+        return crumbData;
+    }
+
+    public void setCrumbData(List<J2735PathHistoryPoint> crumbData) {
+        this.crumbData = crumbData;
+    }
+
 }
--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735SpeedProfile.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735SpeedProfile.java
@@ -6,8 +6,16 @@ import java.util.List;
 import us.dot.its.jpo.ode.plugin.asn1.Asn1Object;
 
 public class J2735SpeedProfile extends Asn1Object {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public List<Integer> speedReports = new ArrayList<Integer>();
+    private List<Integer> speedReports = new ArrayList<>();
+
+    public List<Integer> getSpeedReports() {
+        return speedReports;
+    }
+
+    public void setSpeedReports(List<Integer> speedReports) {
+        this.speedReports = speedReports;
+    }
 
 }
