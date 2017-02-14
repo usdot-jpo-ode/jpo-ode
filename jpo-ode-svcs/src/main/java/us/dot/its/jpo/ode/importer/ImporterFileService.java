92f45bd1a7331b83c0f459bc8b15625b35eab91a
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-02-13 20:15:22 -0500

    Changed ImporterFileService to abstract

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/ImporterFileService.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/ImporterFileService.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-public class ImporterFileService implements ImporterService {
+public abstract class ImporterFileService implements ImporterService {
     
     /**
      * Attempts to create a directory given a String path.
