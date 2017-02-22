d4bd9e440a54099ef4f5c9cf67f785c06830f2d0
Tony Chen <tonychen091@gmail.com>
2017-02-22 13:46:31 -0500

    added in Matt’s code to submit an snmp call

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/traveler/TravelerMessageController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/traveler/TravelerMessageController.java
@@ -1,10 +1,18 @@
 package us.dot.its.jpo.ode.traveler;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.smi.Address;
+import org.snmp4j.smi.GenericAddress;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import us.dot.its.jpo.ode.snmp.SnmpProperties;
+import us.dot.its.jpo.ode.snmp.TimManagerService;
+import us.dot.its.jpo.ode.snmp.TimParameters;
 
 
 @Controller
@@ -26,8 +34,42 @@ public class TravelerMessageController {
 
         System.out.print(timObject.getHexTravelerInformation());
 
+        JSONObject obj = new JSONObject(jsonString);
+        JSONArray rsuList = obj.getJSONArray("RSUs");
 
-        return jsonString;
+        // Needs to be a for loop
+        String ip = rsuList.getJSONObject(0).getString("target");
+
+        String rsuSRMPsid = "8300";
+        int rsuSRMDsrcMsgId = 31;
+        int rsuSRMTxMode = 1;
+        int rsuSRMTxChannel = 178;
+        int rsuSRMTxInterval = 1;
+        String rsuSRMDeliveryStart = "010114111530";
+        String rsuSRMDeliveryStop = "010114130000";
+        String rsuSRMPayload = timObject.getHexTravelerInformation();
+        int rsuSRMEnable = 1;
+        int rsuSRMStatus = 4;
+
+        Address addr = GenericAddress.parse(ip + "/161");
+
+        SnmpProperties testProps = new SnmpProperties(addr, "v3user", "password");
+        TimParameters testParams = new TimParameters(rsuSRMPsid, rsuSRMDsrcMsgId, rsuSRMTxMode, rsuSRMTxChannel,
+                rsuSRMTxInterval, rsuSRMDeliveryStart, rsuSRMDeliveryStop, rsuSRMPayload,
+                rsuSRMEnable, rsuSRMStatus);
+
+        ResponseEvent response = TimManagerService.createAndSend(testParams, testProps);
+
+
+        if (response != null && response.getResponse() != null) {
+
+            return response.getResponse().toString();
+
+        } else {
+
+            return "{\"success\": false}";
+
+        }
     }
     
 }
