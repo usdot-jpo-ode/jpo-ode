aeb2302740764049e37fd20f84e00766ca38dee7
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-03-02 12:28:35 -0500

    ODE-147 Ignored incomplete unit tests

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/dds/DdsRequestManagerTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/dds/DdsRequestManagerTest.java
@@ -3,6 +3,7 @@ package us.dot.its.jpo.ode.dds;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -12,11 +13,13 @@ public class DdsRequestManagerTest {
     
     DdsRequestManager mockedDdsRequestManager;
 
+    @Ignore
     @Before
     public void shouldDoSomething() {
         mockedDdsRequestManager = Mockito.mock(DdsRequestManager.class, Mockito.CALLS_REAL_METHODS);
     }
     
+    @Ignore
     @Test
     public void shouldConstruct() {
         OdeProperties mockProps = Mockito.mock(OdeProperties.class);
