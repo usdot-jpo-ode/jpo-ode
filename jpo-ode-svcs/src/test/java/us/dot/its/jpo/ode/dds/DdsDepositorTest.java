af4d2511051b12eab1d39bf9883080dbb9863f40
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-03-02 14:18:07 -0500

    ODE-147 Added mockito to pom.xml

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/dds/DdsDepositorTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/dds/DdsDepositorTest.java
@@ -17,7 +17,6 @@ import javax.websocket.Session;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.LoggerFactory;
