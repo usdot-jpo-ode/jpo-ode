02d7dc65c4b4a2ea2149e1a1c14c0551ec6d102d
572682 <musavi_hamid@bah.com>
2017-03-03 16:41:40 -0500

    Removed all inactive code from code base.

--- jpo-ode-inactive/src/main/java/us/dot/its/jpo/ode/wrapper/DataPropagator.java
+++
@@ -1,7 +0,0 @@
-package us.dot.its.jpo.ode.wrapper;
-
-import us.dot.its.jpo.ode.model.OdeDataMessage;
-
-public interface DataPropagator<D, R> extends DataProcessor<D, R> {
-   void filterAndSend(OdeDataMessage dataMsg) throws Exception;
-}
