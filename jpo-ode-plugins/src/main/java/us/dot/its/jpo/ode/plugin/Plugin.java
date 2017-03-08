40b4e15db8358628e78d63ceaa8e57f3bcb18e78
572682 <musavi_hamid@bah.com>
2017-03-07 23:29:19 -0500

    ODE-217 Fixed the sonar reported "bug".

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/Plugin.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/Plugin.java
@@ -1,10 +1,11 @@
 package us.dot.its.jpo.ode.plugin;
 
-import java.io.IOException;
 import java.util.Properties;
+
+import us.dot.its.jpo.ode.plugin.OdePluginImpl.OdePluginException;
 
 public interface Plugin {
 
-	void load(Properties properties) throws ClassNotFoundException, IOException;
+	void load(Properties properties) throws OdePluginException;
 
 }
