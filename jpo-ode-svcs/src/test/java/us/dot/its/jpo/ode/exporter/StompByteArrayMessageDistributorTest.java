d60cad4e60767f9e608b44e7ed1fb33960b693bd
585158 <sandesh@terpmail.umd.edu>
2017-03-15 13:26:22 -0400

    modifying stomp test

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/exporter/StompByteArrayMessageDistributorTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/exporter/StompByteArrayMessageDistributorTest.java
@@ -4,23 +4,18 @@ import static org.junit.Assert.fail;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Mocked;
-import mockit.integration.junit4.JMockit;
 import us.dot.its.jpo.ode.plugin.j2735.J2735Bsm;
 import us.dot.its.jpo.ode.subscriber.Subscriber;
 import us.dot.its.jpo.ode.util.SerializationUtils;
 
-@RunWith(JMockit.class)
 public class StompByteArrayMessageDistributorTest {
 
-    @Ignore
     @Test
     public void shouldConstructAndCall(@Injectable SimpMessagingTemplate mockSimpMessagingTemplate,
             @Injectable ConsumerRecord<String, byte[]> mockConsumerRecord,
