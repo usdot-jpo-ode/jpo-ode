2cc3665a7a9cc3086aaec6bf8aeb90f3d1e91d32
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-03-20 12:23:52 -0400

    ODE-227 Restructured pdm objects

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/snmp/PdmManagerService.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/snmp/PdmManagerService.java
@@ -11,7 +11,7 @@ import org.snmp4j.smi.Integer32;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.VariableBinding;
 
-import us.dot.its.jpo.ode.plugin.j2735.J2735ProbeDataManagement.PdmParameters;
+import us.dot.its.jpo.ode.plugin.j2735.pdm.PdmParameters;
 
 public class PdmManagerService {
 
@@ -50,59 +50,57 @@ public class PdmManagerService {
 
     public static ScopedPDU createPDU(PdmParameters params) {
 
+        // Filter null request
         if (params == null) {
             return null;
         }
 
+        ScopedPDU pdu = new ScopedPDU();
+        pdu.setType(PDU.SET);
+
+        // Add singular items to PDU
         VariableBinding rsuPDMSampleStart = new VariableBinding(new OID("1.0.15628.4.1.200.1"),
-                new Integer32(params.getRsuPDMSampleStart()));
+                new Integer32(params.getSampleStart()));
 
         VariableBinding rsuPDMSampleEnd = new VariableBinding(new OID("1.0.15628.4.1.200.2"),
-                new Integer32(params.getRsuPDMSampleEnd()));
+                new Integer32(params.getSampleEnd()));
 
         VariableBinding rsuPDMDirections = new VariableBinding(new OID("1.0.15628.4.1.200.3"),
-                new Integer32(params.getRsuPDMDirections()));
+                new Integer32(params.getDirections()));
 
         VariableBinding rsuPDMTermChoice = new VariableBinding(new OID("1.0.15628.4.1.200.4"),
-                new Integer32(params.getRsuPDMTermChoice()));
+                new Integer32(params.getTermChoice()));
 
         VariableBinding rsuPDMTermTime = new VariableBinding(new OID("1.0.15628.4.1.200.5"),
-                new Integer32(params.getRsuPDMTermTime()));
+                new Integer32(params.getTermTime()));
 
         VariableBinding rsuPDMTermDistance = new VariableBinding(new OID("1.0.15628.4.1.200.6"),
-                new Integer32(params.getRsuPDMTermDistance()));
+                new Integer32(params.getTermDistance()));
 
         VariableBinding rsuPDMSnapshotChoice = new VariableBinding(new OID("1.0.15628.4.1.200.7"),
-                new Integer32(params.getRsuPDMSnapshotChoice()));
+                new Integer32(params.getSnapshotChoice()));
 
         VariableBinding rsuPDMMinSnapshotTime = new VariableBinding(new OID("1.0.15628.4.1.200.9"),
-                new Integer32(params.getRsuPDMMinSnapshotTime()));
+                new Integer32(params.getMinSnapshotTime()));
 
         VariableBinding rsuPDMMaxSnapshotTime = new VariableBinding(new OID("1.0.15628.4.1.200.10"),
-                new Integer32(params.getRsuPDMMaxSnapshotTime()));
+                new Integer32(params.getMaxSnapshotTime()));
 
         VariableBinding rsuPDMMinSnapshotDistnace = new VariableBinding(new OID("1.0.15628.4.1.200.11"),
-                new Integer32(params.getRsuPDMMinSnapshotDistance()));
+                new Integer32(params.getMinSnapshotDistance()));
 
         VariableBinding rsuPDMMaxSnapshotDistnace = new VariableBinding(new OID("1.0.15628.4.1.200.12"),
-                new Integer32(params.getRsuPDMMaxSnapshotDistance()));
+                new Integer32(params.getMaxSnapshotDistance()));
 
         VariableBinding rsuPDMSnapshotMinSpeed = new VariableBinding(new OID("1.0.15628.4.1.200.13"),
-                new Integer32(params.getRsuPDMSnapshotMinSpeed()));
+                new Integer32(params.getSnapshotMinSpeed()));
 
         VariableBinding rsuPDMSnapshotMaxSpeed = new VariableBinding(new OID("1.0.15628.4.1.200.14"),
-                new Integer32(params.getRsuPDMSnapshotMaxSpeed()));
+                new Integer32(params.getSnapshotMaxSpeed()));
 
         VariableBinding rsuPDMTxInterval = new VariableBinding(new OID("1.0.15628.4.1.200.15"),
-                new Integer32(params.getRsuPDMTxInterval()));
+                new Integer32(params.getTxInterval()));
 
-        // for (PdmVehicleStatusEntry entry : params.rsuPDMVSReqListTable) {
-        // // TODO
-        // // create variable binding
-        // // add it to pdu
-        // }
-
-        ScopedPDU pdu = new ScopedPDU();
         pdu.add(rsuPDMSampleStart);
         pdu.add(rsuPDMSampleEnd);
         pdu.add(rsuPDMDirections);
@@ -118,7 +116,32 @@ public class PdmManagerService {
         pdu.add(rsuPDMSnapshotMaxSpeed);
         pdu.add(rsuPDMTxInterval);
 
-        pdu.setType(PDU.SET);
+        // Add request status list items to PDU
+
+        for (int i = 0; i < params.getVehicleStatusRequestList().length; i++) {
+
+            String currentOid = "1.0.15628.4.1.200.15." + i;
+
+            VariableBinding rsuPDMVSReqTag = new VariableBinding(new OID(currentOid + ".1"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getTag()));
+            VariableBinding rsuPDMVSReqSubTag = new VariableBinding(new OID(currentOid + ".2"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getSubTag()));
+            VariableBinding rsuPDMVSReqLessThenValue = new VariableBinding(new OID(currentOid + ".3"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getLessThenValue()));
+            VariableBinding rsuPDMVSReqMoreThenValue = new VariableBinding(new OID(currentOid + ".4"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getMoreThenValue()));
+            VariableBinding rsuPDMVSReqSendAll = new VariableBinding(new OID(currentOid + ".5"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getSendAll()));
+            VariableBinding rsuPDMVSReqStatus = new VariableBinding(new OID(currentOid + ".6"),
+                    new Integer32(params.getVehicleStatusRequestList()[i].getStatus()));
+
+            pdu.add(rsuPDMVSReqTag);
+            pdu.add(rsuPDMVSReqSubTag);
+            pdu.add(rsuPDMVSReqLessThenValue);
+            pdu.add(rsuPDMVSReqMoreThenValue);
+            pdu.add(rsuPDMVSReqSendAll);
+            pdu.add(rsuPDMVSReqStatus);
+        }
 
         return pdu;
     }
