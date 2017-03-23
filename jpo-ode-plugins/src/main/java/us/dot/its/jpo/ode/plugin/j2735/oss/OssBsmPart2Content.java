14fc50bd2525d19dee06cd795f66695b003cc176
585158 <sandesh@terpmail.umd.edu>
2017-03-23 14:30:07 -0400

    ODE-217 Fixed code smells for plugin/j2735/oss/OssBsmPart2Content.java

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssBsmPart2Content.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssBsmPart2Content.java
@@ -1,6 +1,5 @@
 package us.dot.its.jpo.ode.plugin.j2735.oss;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -22,154 +21,172 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735VehicleEventFlags;
 import us.dot.its.jpo.ode.plugin.j2735.J2735VehicleSafetyExtensions;
 
 public class OssBsmPart2Content {
-    
-    private OssBsmPart2Content() {}
 
-    public static class OssBsmPart2Exception extends Exception {
+	private static final String DECODING_ERROR = "Error decoding OpenType value";
+	private static final String NO_OPEN_TYPE = "No OpenType value";
 
-        private static final long serialVersionUID = 7318127023245642955L;
+	private OssBsmPart2Content() {
+	}
 
-        public OssBsmPart2Exception(String msg) {
-            super(msg);
-        }
+	public static class OssBsmPart2Exception extends Exception {
 
-        public OssBsmPart2Exception(String msg, Exception e) {
-            super(msg, e);
-        }
+		private static final long serialVersionUID = 7318127023245642955L;
 
-    }
+		public OssBsmPart2Exception(String msg) {
+			super(msg);
+		}
 
-    public static J2735BsmPart2Content genericPart2Content(
-            us.dot.its.jpo.ode.j2735.dsrc.BasicSafetyMessage.PartII.Sequence_ seq) throws OssBsmPart2Exception {
+		public OssBsmPart2Exception(String msg, Exception e) {
+			super(msg, e);
+		}
 
-        return buildContent(J2735BsmPart2Content.J2735BsmPart2Id.values()[seq.partII_Id.intValue()], seq.partII_Value);
-    }
+	}
 
-    private static J2735BsmPart2Content buildContent(J2735BsmPart2Content.J2735BsmPart2Id id, OpenType value)
-            throws OssBsmPart2Exception {
-        J2735BsmPart2Content part2Content = new J2735BsmPart2Content();
+	public static J2735BsmPart2Content genericPart2Content(
+			us.dot.its.jpo.ode.j2735.dsrc.BasicSafetyMessage.PartII.Sequence_ seq) throws OssBsmPart2Exception {
 
-        part2Content.setId(id);
+		return buildContent(J2735BsmPart2Content.J2735BsmPart2Id.values()[seq.partII_Id.intValue()], seq.partII_Value);
+	}
 
-        PERUnalignedCoder coder = J2735.getPERUnalignedCoder();
+	private static J2735BsmPart2Content buildContent(J2735BsmPart2Content.J2735BsmPart2Id id, OpenType value)
+			throws OssBsmPart2Exception {
+		J2735BsmPart2Content part2Content = new J2735BsmPart2Content();
 
-        switch (part2Content.getId()) {
-        case SPECIALVEHICLEEXT:
-            J2735SpecialVehicleExtensions specVeh = new J2735SpecialVehicleExtensions();
-            part2Content.setValue(specVeh);
+		part2Content.setId(id);
 
-            SpecialVehicleExtensions sp;
-            if (value.getDecodedValue() != null) {
-                sp = (SpecialVehicleExtensions) value.getDecodedValue();
-            } else if (value.getEncodedValueAsStream() != null) {
-                sp = new SpecialVehicleExtensions();
-                try {
-                    coder.decode(value.getEncodedValueAsStream(), sp);
-                } catch (DecodeFailedException | DecodeNotSupportedException e) {
-                    throw new OssBsmPart2Exception("Error decoding OpenType value", e);
-                }
-            } else {
-                throw new OssBsmPart2Exception("No OpenType value");
-            }
+		PERUnalignedCoder coder = J2735.getPERUnalignedCoder();
 
-            if (sp.hasVehicleAlerts()) {
-                specVeh.setVehicleAlerts(OssEmergencyDetails.genericEmergencyDetails(sp.vehicleAlerts));
-            }
-            if (sp.hasDescription()) {
-                specVeh.setDescription(OssEventDescription.genericEventDescription(sp.description));
-            }
-            if (sp.hasTrailers()) {
-                specVeh.setTrailers(OssTrailerData.genericTrailerData(sp.trailers));
-            }
-            break;
-        case SUPPLEMENTALVEHICLEEXT:
-            J2735SupplementalVehicleExtensions supVeh = new J2735SupplementalVehicleExtensions();
-            part2Content.setValue(supVeh);
-            
-            SupplementalVehicleExtensions sve;
-            if (value.getDecodedValue() != null) {
-                sve = (SupplementalVehicleExtensions) value.getDecodedValue();
-            } else if (value.getEncodedValueAsStream() != null) {
-                sve = new SupplementalVehicleExtensions();
-                try {
-                    coder.decode(value.getEncodedValueAsStream(), sve);
-                } catch (DecodeFailedException | DecodeNotSupportedException e) {
-                    throw new OssBsmPart2Exception("Error decoding OpenType value", e);
-                }
-            } else {
-                throw new OssBsmPart2Exception("No OpenType value");
-            }
-            part2Content.setValue(OssSupplementalVehicleExtensions
-                    .genericSupplementalVehicleExtensions(sve));
+		switch (part2Content.getId()) {
+		case SPECIALVEHICLEEXT:
+			evaluateSpecialVehicleExt(part2Content, value, coder);
+			break;
+		case SUPPLEMENTALVEHICLEEXT:
+			evaluateSupplementalVehicleExt(part2Content, value, coder);
+			break;
+		case VEHICLESAFETYEXT:
+			evaluateVehicleSafetyExt(part2Content, value, coder);
+			break;
+		}
+		return part2Content;
+	}
 
-            break;
-        case VEHICLESAFETYEXT:
-            J2735VehicleSafetyExtensions vehSafety = new J2735VehicleSafetyExtensions();
-            part2Content.setValue(vehSafety);
+	private static void evaluateVehicleSafetyExt(J2735BsmPart2Content part2Content, OpenType value,
+			PERUnalignedCoder coder) throws OssBsmPart2Exception {
+		J2735VehicleSafetyExtensions vehSafety = new J2735VehicleSafetyExtensions();
+		part2Content.setValue(vehSafety);
 
-            VehicleSafetyExtensions vse;
-            if (value.getDecodedValue() != null) {
-                vse = (VehicleSafetyExtensions) value.getDecodedValue();
-            } else if (value.getEncodedValueAsStream() != null) {
-                vse = new VehicleSafetyExtensions();
-                try {
-                    coder.decode(value.getEncodedValueAsStream(), vse);
-                } catch (DecodeFailedException | DecodeNotSupportedException e) {
-                    throw new OssBsmPart2Exception("Error decoding OpenType value", e);
-                }
-            } else {
-                throw new OssBsmPart2Exception("No OpenType value");
-            }
+		VehicleSafetyExtensions vse;
+		if (value.getDecodedValue() != null) {
+			vse = (VehicleSafetyExtensions) value.getDecodedValue();
+		} else if (value.getEncodedValueAsStream() != null) {
+			vse = new VehicleSafetyExtensions();
+			try {
+				coder.decode(value.getEncodedValueAsStream(), vse);
+			} catch (DecodeFailedException | DecodeNotSupportedException e) {
+				throw new OssBsmPart2Exception(DECODING_ERROR, e);
+			}
+		} else {
+			throw new OssBsmPart2Exception(NO_OPEN_TYPE);
+		}
 
-            if (vse.hasEvents()) {
-                J2735VehicleEventFlags eventFlags = new J2735VehicleEventFlags();
-                for (int i = 0; i < vse.getEvents().getSize(); i++) {
-                    String flagName = vse.getEvents().getNamedBits().getMemberName(i);
-                    Boolean flagStatus = vse.getEvents().getBit(vse.getEvents().getSize() - i - 1);
+		if (vse.hasEvents()) {
+			J2735VehicleEventFlags eventFlags = new J2735VehicleEventFlags();
+			for (int i = 0; i < vse.getEvents().getSize(); i++) {
+				String flagName = vse.getEvents().getNamedBits().getMemberName(i);
+				Boolean flagStatus = vse.getEvents().getBit(vse.getEvents().getSize() - i - 1);
 
-                    if (flagName != null) {
-                        eventFlags.put(flagName, flagStatus);
-                    }
-                }
-                vehSafety.setEvents(eventFlags);
-            }
-            if (vse.hasLights()) {
-                
-                J2735ExteriorLights exteriorLights = new J2735ExteriorLights();
-                
-                for (int i = 0; i < vse.getLights().getSize(); i++) {
-                    String lightName = vse.getLights().getNamedBits().getMemberName(i);
-                    Boolean lightStatus = vse.getLights().getBit(i);
+				if (flagName != null) {
+					eventFlags.put(flagName, flagStatus);
+				}
+			}
+			vehSafety.setEvents(eventFlags);
+		}
+		if (vse.hasLights()) {
 
-                    if (lightName != null) {
-                        exteriorLights.put(lightName, lightStatus);
-                    }
-                }
-                
-                vehSafety.setLights(exteriorLights);
-                
-            }
-            if (vse.hasPathHistory()) {
-                vehSafety.setPathHistory(OssPathHistory.genericPathHistory(vse.pathHistory));
-            }
-            if (vse.hasPathPrediction()) {
-                vehSafety.setPathPrediction(OssPathPrediction.genericPathPrediction(vse.pathPrediction));
-            }
+			J2735ExteriorLights exteriorLights = new J2735ExteriorLights();
 
-            break;
-        }
-        return part2Content;
-    }
+			for (int i = 0; i < vse.getLights().getSize(); i++) {
+				String lightName = vse.getLights().getNamedBits().getMemberName(i);
+				Boolean lightStatus = vse.getLights().getBit(i);
 
-    public static void buildGenericPart2(ArrayList<Sequence_> elements, List<J2735BsmPart2Content> partII)
-            throws OssBsmPart2Exception {
-        if (elements != null) {
-            Iterator<Sequence_> iter = elements.iterator();
+				if (lightName != null) {
+					exteriorLights.put(lightName, lightStatus);
+				}
+			}
 
-            while (iter.hasNext()) {
-                partII.add(OssBsmPart2Content.genericPart2Content(iter.next()));
-            }
-        }
-    }
+			vehSafety.setLights(exteriorLights);
+
+		}
+		if (vse.hasPathHistory()) {
+			vehSafety.setPathHistory(OssPathHistory.genericPathHistory(vse.pathHistory));
+		}
+		if (vse.hasPathPrediction()) {
+			vehSafety.setPathPrediction(OssPathPrediction.genericPathPrediction(vse.pathPrediction));
+		}
+
+	}
+
+	private static void evaluateSpecialVehicleExt(J2735BsmPart2Content part2Content, OpenType value,
+			PERUnalignedCoder coder) throws OssBsmPart2Exception {
+		J2735SpecialVehicleExtensions specVeh = new J2735SpecialVehicleExtensions();
+		part2Content.setValue(specVeh);
+
+		SpecialVehicleExtensions sp;
+		if (value.getDecodedValue() != null) {
+			sp = (SpecialVehicleExtensions) value.getDecodedValue();
+		} else if (value.getEncodedValueAsStream() != null) {
+			sp = new SpecialVehicleExtensions();
+			try {
+				coder.decode(value.getEncodedValueAsStream(), sp);
+			} catch (DecodeFailedException | DecodeNotSupportedException e) {
+				throw new OssBsmPart2Exception(DECODING_ERROR, e);
+			}
+		} else {
+			throw new OssBsmPart2Exception(NO_OPEN_TYPE);
+		}
+
+		if (sp.hasVehicleAlerts()) {
+			specVeh.setVehicleAlerts(OssEmergencyDetails.genericEmergencyDetails(sp.vehicleAlerts));
+		}
+		if (sp.hasDescription()) {
+			specVeh.setDescription(OssEventDescription.genericEventDescription(sp.description));
+		}
+		if (sp.hasTrailers()) {
+			specVeh.setTrailers(OssTrailerData.genericTrailerData(sp.trailers));
+		}
+	}
+
+	private static void evaluateSupplementalVehicleExt(J2735BsmPart2Content part2Content, OpenType value,
+			PERUnalignedCoder coder) throws OssBsmPart2Exception {
+		J2735SupplementalVehicleExtensions supVeh = new J2735SupplementalVehicleExtensions();
+		part2Content.setValue(supVeh);
+
+		SupplementalVehicleExtensions sve;
+		if (value.getDecodedValue() != null) {
+			sve = (SupplementalVehicleExtensions) value.getDecodedValue();
+		} else if (value.getEncodedValueAsStream() != null) {
+			sve = new SupplementalVehicleExtensions();
+			try {
+				coder.decode(value.getEncodedValueAsStream(), sve);
+			} catch (DecodeFailedException | DecodeNotSupportedException e) {
+				throw new OssBsmPart2Exception(DECODING_ERROR, e);
+			}
+		} else {
+			throw new OssBsmPart2Exception(NO_OPEN_TYPE);
+		}
+		part2Content.setValue(OssSupplementalVehicleExtensions.genericSupplementalVehicleExtensions(sve));
+
+	}
+
+	public static void buildGenericPart2(List<Sequence_> elements, List<J2735BsmPart2Content> partII)
+			throws OssBsmPart2Exception {
+		if (elements != null) {
+			Iterator<Sequence_> iter = elements.iterator();
+
+			while (iter.hasNext()) {
+				partII.add(OssBsmPart2Content.genericPart2Content(iter.next()));
+			}
+		}
+	}
 
 }
