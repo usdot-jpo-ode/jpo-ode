25d41c7e4b224ccb547462b7dcc72ca687fe71fb
mgarramo <GarramoneJr_Michael@bah.com>
2017-03-30 15:24:53 -0400

    restructured TravelerMessageController test

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TravelerMessageControllerTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TravelerMessageControllerTest.java
@@ -1,55 +1,217 @@
 package us.dot.its.jpo.ode.traveler;
 
-import com.oss.asn1.DecodeFailedException;
-import com.oss.asn1.DecodeNotSupportedException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.text.ParseException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.smi.GenericAddress;
 
+import mockit.Expectations;
+import mockit.Injectable;
 import mockit.Mocked;
+import mockit.Tested;
+import mockit.Verifications;
 import mockit.integration.junit4.JMockit;
-import us.dot.its.jpo.ode.OdeProperties;
+import us.dot.its.jpo.ode.ManagerAndControllerServices;
+import us.dot.its.jpo.ode.eventlog.EventLogger;
+import us.dot.its.jpo.ode.plugin.RoadSideUnit.RSU;
+import us.dot.its.jpo.ode.plugin.j2735.J2735TravelerInputData;
+import us.dot.its.jpo.ode.snmp.SnmpProperties;
+import us.dot.its.jpo.ode.snmp.TimParameters;
+import us.dot.its.jpo.ode.util.JsonUtils;
 
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-/**
- * Created by anthonychen on 2/10/17.
- */
 @RunWith(JMockit.class)
 public class TravelerMessageControllerTest {
-
-    @Test
-    public void testBuildTravelerInformation() throws IOException, DecodeFailedException, DecodeNotSupportedException {
-        testBuildTravelerInformation("tim2.json");	// lane
-        testBuildTravelerInformation("tim3.json");	// region
-    }
-    public void testBuildTravelerInformation(String timName) throws IOException, DecodeFailedException, DecodeNotSupportedException {
-//        TravelerMessageController timBuilder = new TravelerMessageController();
-//        String json = FileUtils.readFileToString(new File("src/test/resources/" + timName));
-//        TravelerInformationMessage timMessage = (TravelerInformationMessage)timBuilder.build(json);
-//
-//        assertEquals(TravelerInformationMessage.class.getSimpleName(), timMessage.getMessageName());
-//
-//        TravelerInformation tim = J2735Helper.decodeMessage(timMessage.getHexString(), new TravelerInformation());
-//        System.out.println(tim);
-
-    }
+   @Tested
+   TravelerMessageController tmc;
+   @Mocked
+   J2735TravelerInputData mockTim;
 
    @Mocked
-   private OdeProperties odeProperties;
+   ManagerAndControllerServices mockTimManagerService;
+
+   @Injectable
+   RSU mockRsu;
+   @Mocked
+   ResponseEvent mockResponseEvent;
    
    @Test
-   public void shouldRefuseConnectionNullIp() {
+   public void nullRequestShouldLogAndThrowException(@Mocked final EventLogger eventLogger) {
 
-      String jsonString = null;
+       try {
+           tmc.timMessage(null);
+           fail("Expected timException");
+       } catch (Exception e) {
+           assertEquals(TimMessageException.class, e.getClass());
+           assertEquals("TIM CONTROLLER- Endpoint received null request", e.getMessage());
+       }
 
-      try {
-         new TravelerMessageController(odeProperties).timMessage(jsonString);
-         fail("Expected IllegalArgumentException");
-      } catch (Exception e) {
-         assertEquals(TimMessageException.class, e.getClass());
-      }
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+           }
+       };
+   }
+   
+   @Test
+   public void nullResponseShouldLogAndReturn(@Mocked final JsonUtils jsonUtils) {
+
+       new Expectations() {
+           {
+               JsonUtils.fromJson(anyString, J2735TravelerInputData.class);
+               result = mockTim;
+               
+               mockTim.getRsus();
+               result = new RSU[]{mockRsu};
+               
+               ManagerAndControllerServices.createAndSend((TimParameters)any, (SnmpProperties)any);
+               result = null;
+           }
+       };
+
+       try {
+           assertNotNull(tmc.timMessage("testMessage123"));
+       } catch (Exception e) {
+           fail("Unexpected exception: " + e);
+       }
+
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+           }
+       };
+   }
+   
+   @Test
+   public void nullGetResponseShouldLogAndReturn(@Mocked final JsonUtils jsonUtils) {
+
+       new Expectations() {
+           {
+               JsonUtils.fromJson(anyString, J2735TravelerInputData.class);
+               result = mockTim;
+               
+               mockTim.getRsus();
+               result = new RSU[]{mockRsu};
+               
+               ManagerAndControllerServices.createAndSend((TimParameters)any, (SnmpProperties)any);
+               result = mockResponseEvent;
+               
+               mockResponseEvent.getResponse();
+               result = null;
+           }
+       };
+
+       try {
+           assertNotNull(tmc.timMessage("testMessage123"));
+       } catch (Exception e) {
+           fail("Unexpected exception: " + e);
+       }
+
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+           }
+       };
+   }
+   
+   @Test
+   public void shouldLogSuccessWhenErrorStatus0(@Mocked final JsonUtils jsonUtils) {
+
+       new Expectations() {
+           {
+               JsonUtils.fromJson(anyString, J2735TravelerInputData.class);
+               result = mockTim;
+               
+               mockTim.getRsus();
+               result = new RSU[]{mockRsu};
+               
+               ManagerAndControllerServices.createAndSend((TimParameters)any, (SnmpProperties)any);
+               result = mockResponseEvent;
+               
+               mockResponseEvent.getResponse().getErrorStatus();
+               result = 0;
+           }
+       };
+
+       try {
+           assertNotNull(tmc.timMessage("testMessage123"));
+       } catch (Exception e) {
+           fail("Unexpected exception: " + e);
+       }
+
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+           }
+       };
+   }
+   
+   @Test
+   public void shouldLogFailureWhenErrorStatusNot0(@Mocked final JsonUtils jsonUtils) {
+
+       new Expectations() {
+           {
+               JsonUtils.fromJson(anyString, J2735TravelerInputData.class);
+               result = mockTim;
+               
+               mockTim.getRsus();
+               result = new RSU[]{mockRsu};
+               
+               ManagerAndControllerServices.createAndSend((TimParameters)any, (SnmpProperties)any);
+               result = mockResponseEvent;
+               
+               mockResponseEvent.getResponse().getErrorStatus();
+               result = 17;
+           }
+       };
+
+       try {
+           assertNotNull(tmc.timMessage("testMessage123"));
+       } catch (Exception e) {
+           fail("Unexpected exception: " + e);
+       }
+
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+               mockResponseEvent.getResponse().getErrorStatusText();
+               times = 1;
+           }
+       };
+   }
+   
+   @Test
+   public void shouldCatchParseExceptionAndLog(@Mocked final JsonUtils jsonUtils, @Mocked final GenericAddress genericAddress) {
+
+       new Expectations() {
+           {
+               JsonUtils.fromJson(anyString, J2735TravelerInputData.class);
+               result = mockTim;
+               
+               mockTim.getRsus();
+               result = new RSU[]{mockRsu};
+               
+               GenericAddress.parse(anyString);
+               result = new ParseException("testException123", anyInt);
+           }
+       };
+
+       try {
+           assertNotNull(tmc.timMessage("testMessage123"));
+       } catch (Exception e) {
+           fail("Unexpected exception: " + e);
+       }
+
+       new Verifications() {
+           {
+               EventLogger.logger.info(anyString);
+           }
+       };
    }
 
 }
