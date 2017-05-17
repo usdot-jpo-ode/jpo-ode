9353fe93739fda9af9affe27dfc1899a29538514
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-05-17 14:00:06 -0400

    ODE-308 more unit tests for vsd-->bsm converter

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/vsdm/VsdToBsmConverterTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/vsdm/VsdToBsmConverterTest.java
@@ -1,33 +1,94 @@
 package us.dot.its.jpo.ode.vsdm;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Test;
 
+import us.dot.its.jpo.ode.j2735.dsrc.AccelerationSet4Way;
+import us.dot.its.jpo.ode.j2735.dsrc.BasicSafetyMessage;
+import us.dot.its.jpo.ode.j2735.dsrc.BrakeSystemStatus;
+import us.dot.its.jpo.ode.j2735.dsrc.DDateTime;
+import us.dot.its.jpo.ode.j2735.dsrc.Elevation;
+import us.dot.its.jpo.ode.j2735.dsrc.Heading;
+import us.dot.its.jpo.ode.j2735.dsrc.Latitude;
+import us.dot.its.jpo.ode.j2735.dsrc.Longitude;
 import us.dot.its.jpo.ode.j2735.dsrc.MsgCRC;
+import us.dot.its.jpo.ode.j2735.dsrc.Position3D;
+import us.dot.its.jpo.ode.j2735.dsrc.SteeringWheelAngle;
+import us.dot.its.jpo.ode.j2735.dsrc.TemporaryID;
+import us.dot.its.jpo.ode.j2735.dsrc.TransmissionAndSpeed;
+import us.dot.its.jpo.ode.j2735.dsrc.VehicleSize;
+import us.dot.its.jpo.ode.j2735.dsrc.Velocity;
+import us.dot.its.jpo.ode.j2735.semi.FundamentalSituationalStatus;
 import us.dot.its.jpo.ode.j2735.semi.GroupID;
 import us.dot.its.jpo.ode.j2735.semi.SemiDialogID;
 import us.dot.its.jpo.ode.j2735.semi.VehSitDataMessage;
 import us.dot.its.jpo.ode.j2735.semi.VehSitDataMessage.Bundle;
+import us.dot.its.jpo.ode.j2735.semi.VehSitRecord;
 
 public class VsdToBsmConverterTest {
-	
-	@Ignore @Before
-	public void setUp() {
-		fail("Not yet implemented");
+
+	VehSitDataMessage validVsdm;
+
+	@Before
+	public void createTestVsdm() {
+
+		validVsdm = new VehSitDataMessage();
+		validVsdm.bundle = new Bundle();
+		validVsdm.bundle.elements = new ArrayList<>();
+		validVsdm.crc = new MsgCRC(new byte[] { 1 });
+		validVsdm.dialogID = new SemiDialogID(2);
+		validVsdm.groupID = new GroupID(new byte[] { 3 });
+
+		VehSitRecord vsr = new VehSitRecord();
+		vsr.tempID = new TemporaryID();
+		vsr.time = new DDateTime();
+		vsr.pos = new Position3D();
+		vsr.pos._long = new Longitude(90);
+		vsr.pos.lat = new Latitude(90);
+		vsr.pos.elevation = new Elevation();
+		vsr.fundamental = new FundamentalSituationalStatus();
+		vsr.fundamental.accelSet = new AccelerationSet4Way();
+		vsr.fundamental.steeringAngle = new SteeringWheelAngle();
+		vsr.fundamental.brakes = new BrakeSystemStatus();
+		vsr.fundamental.heading = new Heading();
+		vsr.fundamental.vehSize = new VehicleSize();
+		vsr.fundamental.speed = new TransmissionAndSpeed();
+		vsr.fundamental.speed.speed = new Velocity(8191);
 		
+		validVsdm.bundle.add(vsr);
+	}
+
+	@Test
+	public void shouldThrowExceptionBundleNull() {
+		VehSitDataMessage vsdm = new VehSitDataMessage();
+		try {
+			VsdToBsmConverter.convert(vsdm);
+			fail("Expected IllegalArgumentException.");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
+	}
+
+	@Test
+	public void shouldReturnEmptyListFromEmptyBundle() {
 		VehSitDataMessage vsdm = new VehSitDataMessage();
 		vsdm.bundle = new Bundle();
-		vsdm.bundle.elements = new ArrayList<>();
-		vsdm.crc = new MsgCRC(new byte[]{1});
-		vsdm.dialogID = new SemiDialogID(2);
-		vsdm.groupID = new GroupID(new byte[]{3});
+		assertTrue(VsdToBsmConverter.convert(vsdm).size() == 0);
+	}
+
+	@Test
+	public void shouldConvertCorrectly() {
+		List<BasicSafetyMessage> bsmlist = VsdToBsmConverter.convert(validVsdm);
+		assertTrue(bsmlist.size() == 1);
 		
-		//TODO
-		
+		BasicSafetyMessage bsm = bsmlist.get(0);
+		assertTrue(bsm.coreData._long.intValue() == 90);
 	}
 
 }
