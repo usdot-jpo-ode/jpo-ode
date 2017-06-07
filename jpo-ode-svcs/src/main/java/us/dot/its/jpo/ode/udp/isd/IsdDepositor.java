dfd602ddcf32640c19cd354cc69050e3c04832f2
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-06-07 14:32:02 -0400

    ODE-314 isd debug

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/isd/IsdDepositor.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/isd/IsdDepositor.java
@@ -9,31 +9,27 @@ import us.dot.its.jpo.ode.dds.AbstractSubscriberDepositor;
 import us.dot.its.jpo.ode.j2735.semi.SemiDialogID;
 
 public class IsdDepositor extends AbstractSubscriberDepositor<String, byte[]> {
-    
+
 	public IsdDepositor(OdeProperties odeProps) {
-        super(odeProps, odeProps.getIsdDepositorPort(), SemiDialogID.intersectionSitDataDep);
+		super(odeProps, odeProps.getIsdDepositorPort(), SemiDialogID.intersectionSitDataDep);
 	}
 
-    @Override
-    protected byte[] deposit() {
-        /* 
-         * The record.value() will return an encoded ISD 
-         */
-        byte[] encodedIsd = record.value();
+	@Override
+	protected byte[] deposit() {
+		/*
+		 * The record.value() will return an encoded ISD
+		 */
+		byte[] encodedIsd = record.value();
 
-        try {
-            logger.debug(
-                    "Sending Isd to SDC IP: {} Port: {}",
-                    odeProperties.getSdcIp(),
-                    odeProperties.getSdcPort());
-            socket.send(new DatagramPacket(
-                    encodedIsd,
-                    encodedIsd.length,
-                        new InetSocketAddress(odeProperties.getSdcIp(), odeProperties.getSdcPort())));
-        } catch (IOException e) {
-            logger.error("Error Sending Isd to SDC", e);
-        }
-        return encodedIsd;
-    }
+		try {
+			logger.debug("Sending Isd to SDC IP: {}:{} from port: {}", odeProperties.getSdcIp(),
+					odeProperties.getSdcPort(), socket.getLocalPort());
+			socket.send(new DatagramPacket(encodedIsd, encodedIsd.length,
+					new InetSocketAddress(odeProperties.getSdcIp(), odeProperties.getSdcPort())));
+		} catch (IOException e) {
+			logger.error("Error Sending Isd to SDC", e);
+		}
+		return encodedIsd;
+	}
 
 }
