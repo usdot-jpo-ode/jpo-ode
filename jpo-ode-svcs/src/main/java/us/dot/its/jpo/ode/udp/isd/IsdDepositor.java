dfebe2ca8fd0cae679cb3c9a0d7d17fa07649232
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-06-08 11:53:16 -0400

    ODE-314 added isd acceptance step to depositor

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/isd/IsdDepositor.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/isd/IsdDepositor.java
@@ -1,12 +1,27 @@
 package us.dot.its.jpo.ode.udp.isd;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+import com.oss.asn1.EncodeFailedException;
+import com.oss.asn1.EncodeNotSupportedException;
+import com.oss.asn1.INTEGER;
+import com.oss.asn1.PERUnalignedCoder;
 
 import us.dot.its.jpo.ode.OdeProperties;
+import us.dot.its.jpo.ode.asn1.j2735.J2735Util;
 import us.dot.its.jpo.ode.dds.AbstractSubscriberDepositor;
+import us.dot.its.jpo.ode.j2735.J2735;
+import us.dot.its.jpo.ode.j2735.dsrc.TemporaryID;
+import us.dot.its.jpo.ode.j2735.semi.DataReceipt;
+import us.dot.its.jpo.ode.j2735.semi.GroupID;
+import us.dot.its.jpo.ode.j2735.semi.IntersectionSituationDataAcceptance;
 import us.dot.its.jpo.ode.j2735.semi.SemiDialogID;
+import us.dot.its.jpo.ode.j2735.semi.SemiSequenceID;
 
 public class IsdDepositor extends AbstractSubscriberDepositor<String, byte[]> {
 
@@ -29,6 +44,43 @@ public class IsdDepositor extends AbstractSubscriberDepositor<String, byte[]> {
 		} catch (IOException e) {
 			logger.error("Error Sending Isd to SDC", e);
 		}
+		
+		/*
+		 * Send an ISDAcceptance message to confirm deposit
+		 */
+		IntersectionSituationDataAcceptance acceptance = new IntersectionSituationDataAcceptance();
+		acceptance.dialogID = dialogId;
+		acceptance.groupID = new GroupID(OdeProperties.JPO_ODE_GROUP_ID);
+		acceptance.requestID = new TemporaryID(ByteBuffer.allocate(4).putInt(new Random().nextInt(256)).array());
+		acceptance.seqID = SemiSequenceID.accept;
+		acceptance.recordsSent = new INTEGER(1);
+		
+		
+		ByteArrayOutputStream sink = new ByteArrayOutputStream();
+		try {
+			J2735.getPERUnalignedCoder().encode(acceptance, sink);
+		} catch (EncodeFailedException | EncodeNotSupportedException e) {
+			logger.error("Error encoding ISD Acceptance message", e);
+		}
+		byte[] encodedAccept = sink.toByteArray();
+		
+		try {
+			logger.debug("Sending ISD Acceptance message to SDC.");
+			socket.send(new DatagramPacket(encodedAccept, encodedAccept.length,
+					new InetSocketAddress(odeProperties.getSdcIp(), odeProperties.getSdcPort())));
+		} catch (IOException e) {
+			logger.error("Error sending ISD Acceptance message to SDC", e);
+		}
+		
+		try {
+			byte[] buffer = new byte[odeProperties.getServiceResponseBufferSize()];
+			logger.debug("Waiting for ISD Data Receipt from SDC...");
+			DatagramPacket responseDp = new DatagramPacket(buffer, buffer.length);
+			socket.receive(responseDp);
+		} catch (IOException e) {
+			logger.error("Error receiving ISD Receipt message to SDC", e);
+		}
+
 		return encodedIsd;
 	}
 
