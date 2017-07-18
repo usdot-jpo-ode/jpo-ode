939a2525a19d380cfa58eb99f040f62bb0e9442a
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-07-18 09:00:34 -0400

    ODE-314 bugfix

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackagerController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackagerController.java
@@ -10,7 +10,8 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735Bsm;
 import us.dot.its.jpo.ode.wrapper.MessageConsumer;
 import us.dot.its.jpo.ode.wrapper.MessageProducer;
 
-@Controller
+// TODO fix this class 
+// @Controller
 public class BsmToVsdPackagerController {
 
    private static final Logger logger = LoggerFactory.getLogger(BsmToVsdPackagerController.class);
