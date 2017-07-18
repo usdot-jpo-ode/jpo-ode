0428d137b0d8e7fdea3bea74b6f4de746288d93a
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-07-18 10:11:43 -0400

    ODE-314 bugfix

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackagerController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackagerController.java
@@ -28,10 +28,10 @@ public class BsmToVsdPackagerController {
          logger.info("Converting {} records from topic {} and publishing to topic {} ", J2735Bsm.class.getSimpleName(),
                inputTopic, outputTopic);
 
-         BsmToVsdPackager<byte[]> converter = new BsmToVsdPackager<>(MessageProducer.defaultByteArrayMessageProducer(
+         BsmToVsdPackager<String> converter = new BsmToVsdPackager<>(MessageProducer.defaultByteArrayMessageProducer(
                odeProps.getKafkaBrokers(), odeProps.getKafkaProducerType()), outputTopic);
 
-         MessageConsumer<String, byte[]> consumer = new MessageConsumer<>(odeProps.getKafkaBrokers(),
+         MessageConsumer<String, String> consumer = new MessageConsumer<>(odeProps.getKafkaBrokers(),
                this.getClass().getSimpleName(), converter);
 
          consumer.setName(this.getClass().getSimpleName());
