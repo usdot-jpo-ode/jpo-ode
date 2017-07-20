8ae8230cd5a40efdb434114a5529c5d18a3d1b83
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-07-20 18:28:05 -0400

    ODE-314 isd receiver unit tests

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/controller/UdpServicesControllerTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/controller/UdpServicesControllerTest.java
@@ -12,20 +12,32 @@ import mockit.Mocked;
 import us.dot.its.jpo.ode.OdeProperties;
 import us.dot.its.jpo.ode.dds.AbstractSubscriberDepositor;
 import us.dot.its.jpo.ode.udp.AbstractUdpReceiverPublisher;
+import us.dot.its.jpo.ode.udp.vsd.VsdDepositor;
 
 public class UdpServicesControllerTest {
 
+   @Mocked
+   OdeProperties mockOdeProperties;
+
+   @Capturing
+   ServiceManager capturingServiceManager;
+   @Mocked
+   ServiceManager mockServiceManager;
+   @Capturing
+   AbstractUdpReceiverPublisher capturingAbstractUdpReceiverPublisher;
+   @Capturing
+   AbstractSubscriberDepositor capturingAbstractSubscriberDepositor;
+   @Capturing
+   VsdDepositor capturingVsdDepositor;
+
    @Test
-   public void shouldSubmitFiveThreads(@Mocked OdeProperties mockOdeProperties,
-         @Capturing ServiceManager capturingServiceManager, @Mocked ServiceManager mockServiceManager,
-         @Capturing AbstractUdpReceiverPublisher capturingAbstractUdpReceiverPublisher,
-         @Capturing AbstractSubscriberDepositor capturingAbstractSubscriberDepositor) {
+   public void vsdOnShouldSubmit5ThreadsVsdOn() {
 
       new Expectations() {
          {
             mockOdeProperties.getDepositSanitizedBsmToSdc();
             result = true;
-            
+
             new ServiceManager((ThreadFactory) any);
             result = mockServiceManager;
 
@@ -34,6 +46,31 @@ public class UdpServicesControllerTest {
 
             mockServiceManager.submit((AbstractSubscriberDepositor) any, anyString);
             times = 2;
+         }
+      };
+
+      assertNotNull(new UdpServicesController(mockOdeProperties));
+   }
+
+   @Test
+   public void vsdOnShouldSubmit4ThreadsVsdOff() {
+
+      new Expectations() {
+         {
+            mockOdeProperties.getDepositSanitizedBsmToSdc();
+            result = false;
+
+            new ServiceManager((ThreadFactory) any);
+            result = mockServiceManager;
+
+            mockServiceManager.submit((AbstractUdpReceiverPublisher) any);
+            times = 3;
+
+            mockServiceManager.submit((AbstractSubscriberDepositor) any, anyString);
+            times = 1;
+
+            new VsdDepositor((OdeProperties) any);
+            times = 0;
          }
       };
 
