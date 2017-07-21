2a22a3b5d8443dc63350d3417909d8a0e9d38e6d
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-07-21 08:46:20 -0400

    ODE-314 bsmtovsdpackager unit tests

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackager.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/vsd/BsmToVsdPackager.java
@@ -1,9 +1,5 @@
 package us.dot.its.jpo.ode.services.vsd;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.apache.tomcat.util.buf.HexUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,21 +8,9 @@ import com.oss.asn1.EncodeFailedException;
 import com.oss.asn1.EncodeNotSupportedException;
 import com.oss.asn1.PERUnalignedCoder;
 
-import us.dot.its.jpo.ode.OdeProperties;
 import us.dot.its.jpo.ode.j2735.J2735;
-import us.dot.its.jpo.ode.j2735.dsrc.MsgCRC;
-import us.dot.its.jpo.ode.j2735.dsrc.TemporaryID;
-import us.dot.its.jpo.ode.j2735.semi.GroupID;
-import us.dot.its.jpo.ode.j2735.semi.SemiDialogID;
-import us.dot.its.jpo.ode.j2735.semi.SemiSequenceID;
 import us.dot.its.jpo.ode.j2735.semi.VehSitDataMessage;
-import us.dot.its.jpo.ode.j2735.semi.VehSitDataMessage.Bundle;
-import us.dot.its.jpo.ode.j2735.semi.VehSitRecord;
-import us.dot.its.jpo.ode.j2735.semi.VsmType;
 import us.dot.its.jpo.ode.plugin.j2735.J2735Bsm;
-import us.dot.its.jpo.ode.plugin.j2735.oss.OssVehicleSituationRecord;
-import us.dot.its.jpo.ode.udp.bsm.BsmComparator;
-import us.dot.its.jpo.ode.util.CodecUtils;
 import us.dot.its.jpo.ode.util.JsonUtils;
 import us.dot.its.jpo.ode.wrapper.AbstractSubPubTransformer;
 import us.dot.its.jpo.ode.wrapper.MessageProducer;
@@ -41,22 +25,18 @@ public class BsmToVsdPackager extends AbstractSubPubTransformer<String, String, 
 
    private static final Logger logger = LoggerFactory.getLogger(BsmToVsdPackager.class);
 
-   private static final int VSD_PACKAGE_SIZE = 10;
    private final PERUnalignedCoder coder;
-   private ConcurrentHashMap<String, Queue<J2735Bsm>> bsmQueueMap;
+
+   private VsdBundler bundler;
 
    public BsmToVsdPackager(MessageProducer<String, byte[]> producer, String outputTopic) {
       super(producer, (java.lang.String) outputTopic);
       this.coder = J2735.getPERUnalignedCoder();
-      this.bsmQueueMap = new ConcurrentHashMap<>();
+      this.bundler = new VsdBundler();
    }
 
    @Override
    protected byte[] transform(String consumedData) {
-
-      if (null == consumedData) {
-         return new byte[0];
-      }
 
       J2735Bsm bsmData = (J2735Bsm) JsonUtils.fromJson(consumedData, J2735Bsm.class);
 
@@ -64,7 +44,7 @@ public class BsmToVsdPackager extends AbstractSubPubTransformer<String, String, 
       try {
          logger.debug("Consuming BSM.");
 
-         VehSitDataMessage vsd = addToVsdBundle(bsmData);
+         VehSitDataMessage vsd = bundler.addToVsdBundle(bsmData);
 
          // Only full VSDs (10) will be published
          // TODO - toggleable mechanism for periodically publishing not-full
@@ -78,47 +58,5 @@ public class BsmToVsdPackager extends AbstractSubPubTransformer<String, String, 
          logger.error("Error Sending VSD to SDC", e);
       }
       return encodedVsd;
-   }
-
-   private VehSitDataMessage addToVsdBundle(J2735Bsm j2735Bsm) {
-
-      String tempId = j2735Bsm.getCoreData().getId();
-      if (!bsmQueueMap.containsKey(tempId)) {
-         logger.info("Creating new VSD package queue for BSMs with tempID {} to VSD package queue", tempId);
-         Queue<J2735Bsm> bsmQueue = new PriorityQueue<>(VSD_PACKAGE_SIZE, new BsmComparator());
-         bsmQueueMap.put(tempId, bsmQueue);
-      }
-
-      bsmQueueMap.get(tempId).add(j2735Bsm);
-
-      // After receiving enough messages, craft the VSD and return it
-      if (bsmQueueMap.get(tempId).size() == VSD_PACKAGE_SIZE) {
-
-         logger.info("BSM queue ID {} full, crafting VSD", tempId);
-
-         // convert the BSMs in the priority queue to VSRs to craft VSD bundle
-         Bundle vsrBundle = new Bundle();
-         Queue<J2735Bsm> bsmArray = bsmQueueMap.get(tempId);
-         for (J2735Bsm entry : bsmArray) {
-            VehSitRecord vsr = OssVehicleSituationRecord.convertBsmToVsr(entry);
-            vsrBundle.add(vsr);
-         }
-
-         VehSitDataMessage vsd = new VehSitDataMessage();
-         vsd.bundle = vsrBundle;
-         vsd.dialogID = SemiDialogID.vehSitData;
-         vsd.seqID = SemiSequenceID.data;
-         vsd.groupID = new GroupID(OdeProperties.getJpoOdeGroupId());
-         vsd.requestID = new TemporaryID(CodecUtils.fromHex(tempId));
-         vsd.crc = new MsgCRC(new byte[] { 0, 0 });
-         vsd.type = new VsmType(new byte[] { 1 }); // 1=fundamental sit. status
-
-         bsmQueueMap.remove(tempId); // prevent duplicates
-         return vsd;
-      } else {
-         logger.info("Added BSM with tempID {} to existing VSD package queue ({}/{})", tempId,
-               bsmQueueMap.get(tempId).size(), VSD_PACKAGE_SIZE);
-         return null;
-      }
    }
 }
