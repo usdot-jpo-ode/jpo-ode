857a25b9c670f1cea2e81892e8c4d4f2230c2642
572682 <musavi_hamid@bah.com>
2017-07-25 13:21:18 -0400

    ODE-436 Minor changes while trying to fix the unexplained unit test issues

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/dds/DdsRequestManager.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/dds/DdsRequestManager.java
@@ -37,7 +37,7 @@ public abstract class DdsRequestManager<T> {
                this.odeProperties.getDdsCasPassword(), this.odeProperties.getDdsWebsocketUrl(), null, null);
 
       } catch (Exception e) {
-         throw new DdsRequestManagerException("Error initializing DdsRequestManager" + e);
+         throw new DdsRequestManagerException("Error initializing DdsRequestManager", e);
       }
    }
 
