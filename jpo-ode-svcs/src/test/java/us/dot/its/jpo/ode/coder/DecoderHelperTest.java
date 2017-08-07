281c064ba526d4552774fc5cebb9c63e374fa889
levesque1 <levesque_paul@bah.com>
2017-08-07 15:58:50 -0400

    added tests to MessagePublisher

---
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/coder/DecoderHelperTest.java
@@ -0,0 +1,16 @@
+package us.dot.its.jpo.ode.coder;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DecoderHelperTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
