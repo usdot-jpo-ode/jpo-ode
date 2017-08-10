bca569989f83a55379174cd1b1a3396bc8fbe9d6
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-08-10 11:20:27 -0400

    ODE-314 fixed error in vsr converter

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssVehicleSituationRecord.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/oss/OssVehicleSituationRecord.java
@@ -249,7 +249,7 @@ public class OssVehicleSituationRecord {
       if (null == jAcc) {
          nAcc = new Acceleration(2001);
       } else {
-         nAcc = new Acceleration((int) Math.floor(jAcc.doubleValue() / 100));
+         nAcc = new Acceleration(jAcc.multiply(BigDecimal.valueOf(100)).intValue());
       }
       return nAcc;
    }
@@ -266,7 +266,7 @@ public class OssVehicleSituationRecord {
       if (null == jVerta) {
          nVerta = new VerticalAcceleration(-127);
       } else {
-         nVerta = new VerticalAcceleration((int) Math.floor(jVerta.doubleValue() / 0.02));
+         nVerta = new VerticalAcceleration(jVerta.multiply(BigDecimal.valueOf(50)).intValue());
       }
       return nVerta;
    }
