f205b876794e3a2dfb39cd41caa901ec43d266cd
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-09-11 14:22:45 -0400

    ODE-512 more bsm file parser unit tests

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/importer/BsmFileParserTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/importer/BsmFileParserTest.java
@@ -17,24 +17,129 @@ public class BsmFileParserTest {
    @Tested
    BsmFileParser testBsmFileParser;
 
+   /**
+    * Step 0 test. Test an empty stream should immediately return EOF, but still
+    * set the filename.
+    */
    @Test
-   public void testEmptyInputStreamShouldReturnEOF() {
+   public void testStep0() {
+
+      String testFileName = "testLogFile.bin";
+      ParserStatus expectedStatus = ParserStatus.EOF;
+      int expectedStep = 1;
+
+      BufferedInputStream testInputStream = new BufferedInputStream(new ByteArrayInputStream(new byte[0]));
+
       try {
-         assertEquals(ParserStatus.EOF, testBsmFileParser
-               .parse(new BufferedInputStream(new ByteArrayInputStream(new byte[0])), "testLogFile.bin"));
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals(testFileName, testBsmFileParser.getFilename());
+         assertEquals(expectedStep, testBsmFileParser.getStep());
       } catch (LogFileParserException e) {
          fail("Unexpected exception: " + e);
       }
    }
 
+   /**
+    * Step 1 test. Should extract the "direction" value, length 1 byte, then
+    * return EOF.
+    */
    @Test
-   public void testSingleByteInputStreamShouldReturnPartial() {
+   public void testStep1() {
+
+      int testDirection = 1;
+      ParserStatus expectedStatus = ParserStatus.EOF;
+      BsmSource expectedDirection = BsmSource.RV_RX;
+      int expectedStep = 2;
+
+      BufferedInputStream testInputStream = new BufferedInputStream(
+            new ByteArrayInputStream(new byte[] { (byte) testDirection }));
+
       try {
-         assertEquals(ParserStatus.PARTIAL, testBsmFileParser
-               .parse(new BufferedInputStream(new ByteArrayInputStream(new byte[2])), "testLogFile.bin"));
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals(expectedDirection, testBsmFileParser.getDirection());
+         assertEquals(expectedStep, testBsmFileParser.getStep());
       } catch (LogFileParserException e) {
          fail("Unexpected exception: " + e);
       }
    }
+  
+   /**
+    * Step 2 test. Should extract UTC time, 4 byte value, then return EOF.
+    */
+   @Test
+   public void testtStep2() {
+      ParserStatus expectedStatus = ParserStatus.EOF;
+      long expectedUtcTime = 33752069L; // 5,4,3,2 backwards as bytes
+      int expectedStep = 3;
 
+      BufferedInputStream testInputStream = new BufferedInputStream(
+            new ByteArrayInputStream(new byte[] { 0, 5, 4, 3, 2 }));
+
+      try {
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals(expectedUtcTime, testBsmFileParser.getUtctimeInSec());
+         assertEquals(expectedStep, testBsmFileParser.getStep());
+      } catch (LogFileParserException e) {
+         fail("Unexpected exception: " + e);
+      }
+   }
+   
+   /**
+    * Step 2 test without enough bytes, should return PARTIAL
+    */
+   @Test
+   public void testStep2Partial() {
+      ParserStatus expectedStatus = ParserStatus.PARTIAL;
+      int expectedStep = 2;
+
+      BufferedInputStream testInputStream = new BufferedInputStream(
+            new ByteArrayInputStream(new byte[] { 0, 5, 4, 3 }));
+
+      try {
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals("Wrong step", expectedStep, testBsmFileParser.getStep());
+      } catch (LogFileParserException e) {
+         fail("Unexpected exception: " + e);
+      }
+   }
+   
+   /**
+    * Step 3 test. Should extract millisecond offset
+    */
+   @Test
+   public void tetstStep3() {
+      ParserStatus expectedStatus = ParserStatus.EOF;
+      short expectedMsec = 0x0C0F; // 15, 12 backwards as bytes
+      int expectedStep = 4;
+
+      BufferedInputStream testInputStream = new BufferedInputStream(
+            new ByteArrayInputStream(new byte[] { 1, 5, 4, 3, 2, 0xF, 0xC }));
+
+      try {
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals(expectedMsec, testBsmFileParser.getmSec());
+         assertEquals("Wrong step", expectedStep, testBsmFileParser.getStep());
+      } catch (LogFileParserException e) {
+         fail("Unexpected exception: " + e);
+      }
+   }
+   
+   /**
+    * Step 3 partial test
+    */
+   @Test
+   public void tetstStep3Partial() {
+      ParserStatus expectedStatus = ParserStatus.PARTIAL;
+      int expectedStep = 3;
+
+      BufferedInputStream testInputStream = new BufferedInputStream(
+            new ByteArrayInputStream(new byte[] { 1, 5, 4, 3, 2, 0xF }));
+
+      try {
+         assertEquals(expectedStatus, testBsmFileParser.parse(testInputStream, "testLogFile.bin"));
+         assertEquals("Wrong step", expectedStep, testBsmFileParser.getStep());
+      } catch (LogFileParserException e) {
+         fail("Unexpected exception: " + e);
+      }
+   }
 }
