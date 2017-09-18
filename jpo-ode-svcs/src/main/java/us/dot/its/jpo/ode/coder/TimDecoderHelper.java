83f19f92821d377c30a9c20e9341cabf006d8ec2
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-09-18 15:29:04 -0400

    ODE-459 tim extraction now working

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/TimDecoderHelper.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/TimDecoderHelper.java
@@ -1,11 +1,13 @@
 package us.dot.its.jpo.ode.coder;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import org.apache.tomcat.util.buf.HexUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.asn1.AbstractData;
 import com.oss.asn1.DecodeFailedException;
 import com.oss.asn1.DecodeNotSupportedException;
 import com.oss.asn1.EncodeFailedException;
@@ -20,6 +22,7 @@ import gov.usdot.cv.security.msg.MessageException;
 import us.dot.its.jpo.ode.importer.parser.RxMsgFileParser;
 import us.dot.its.jpo.ode.j2735.J2735;
 import us.dot.its.jpo.ode.j2735.dsrc.BasicSafetyMessage;
+import us.dot.its.jpo.ode.j2735.dsrc.DSRCmsgID;
 import us.dot.its.jpo.ode.j2735.dsrc.MessageFrame;
 import us.dot.its.jpo.ode.j2735.dsrc.TravelerInformation;
 import us.dot.its.jpo.ode.model.OdeData;
@@ -31,12 +34,15 @@ import us.dot.its.jpo.ode.model.SerialId;
 import us.dot.its.jpo.ode.plugin.j2735.J2735TravelerInformationMessage;
 import us.dot.its.jpo.ode.plugin.j2735.oss.Oss1609dot2Coder;
 import us.dot.its.jpo.ode.plugin.j2735.oss.OssJ2735Coder;
+import us.dot.its.jpo.ode.plugin.j2735.oss.OssMessageFrame.OssMessageFrameException;
 import us.dot.its.jpo.ode.security.SecurityManager;
 import us.dot.its.jpo.ode.security.SecurityManager.SecurityManagerException;
 
 public class TimDecoderHelper {
 
    private static final Logger logger = LoggerFactory.getLogger(TimDecoderHelper.class);
+   private static final int DSRC_MSG_ID_BSM = 20;
+   private static final int DSRC_MSG_ID_TIM = 31;
    private PERUnalignedCoder asnDecoder;
    private Oss1609dot2Coder ieee1609dotCoder;
    private RawBsmMfSorter rawBsmMfSorter;
@@ -50,12 +56,13 @@ public class TimDecoderHelper {
    public OdeData decode(RxMsgFileParser fileParser, SerialId serialId) throws Exception {
 
       Ieee1609Dot2Data ieee1609dot2Data = ieee1609dotCoder.decodeIeee1609Dot2DataBytes(fileParser.getPayload());
-      OdeObject tim = null;
+      OdeObject odeTim = null;
       OdeData odeTimData = null;
       IEEE1609p2Message message = null;
+      
+      
 
-      TravelerInformation timMessage = new TravelerInformation();
-      BasicSafetyMessage bsmMessage = new BasicSafetyMessage();
+      AbstractData decodedMessage = null;
       MessageFrame mf = new MessageFrame();
 
       if (ieee1609dot2Data != null) {
@@ -77,7 +84,7 @@ public class TimDecoderHelper {
             byte[] unsecuredDataContent = Ieee1609ContentValidator.getUnsecuredData(ieee1609dot2Data.getContent());
             try {
                if (unsecuredDataContent != null) {
-                  logger.info("THIS SHOULD BE A TIM: {}", HexUtils.toHexString(unsecuredDataContent));
+                  logger.debug("Extracted TIM: {}", HexUtils.toHexString(unsecuredDataContent));
                   asnDecoder.decode(ByteBuffer.wrap(unsecuredDataContent), mf);
                }
             } catch (DecodeNotSupportedException | DecodeFailedException e2) {
@@ -94,15 +101,61 @@ public class TimDecoderHelper {
          asnDecoder.decode(ByteBuffer.wrap(fileParser.getPayload()), mf);
       }
 
-      logger.debug("Extracted a TIM: " + timMessage);
+      
+      
+      if (mf != null) {
+         if (mf.getMessageId().intValue() == DSRC_MSG_ID_TIM) {
+            
+            TravelerInformation tim;
+            if (mf.value.getDecodedValue() != null) {
+               tim = (TravelerInformation) mf.value.getDecodedValue();
+            } else if (mf.value.getEncodedValueAsStream() != null) {
+               tim = new TravelerInformation();
+                try {
+                   asnDecoder.decode(mf.value.getEncodedValueAsStream(), tim);
+                } catch (DecodeFailedException | DecodeNotSupportedException e) {
+                    throw new OssMessageFrameException("Error decoding OpenType value", e);
+                }
+            } else {
+                throw new OssMessageFrameException("No OpenType value");
+            }
+
+            logger.debug("Extracted a TIM: " + tim);
+            decodedMessage = tim;
+            
+            
+            
+         } else if (mf.getMessageId().intValue() == DSRC_MSG_ID_BSM) {
+            
+            BasicSafetyMessage bsm;
+            if (mf.value.getDecodedValue() != null) {
+                bsm = (BasicSafetyMessage) mf.value.getDecodedValue();
+            } else if (mf.value.getEncodedValueAsStream() != null) {
+                bsm = new BasicSafetyMessage();
+                try {
+                   asnDecoder.decode(mf.value.getEncodedValueAsStream(), bsm);
+                } catch (DecodeFailedException | DecodeNotSupportedException e) {
+                    throw new OssMessageFrameException("Error decoding OpenType value", e);
+                }
+            } else {
+                throw new OssMessageFrameException("No OpenType value");
+            }
+
+            logger.debug("Extracted a BSM: " + bsm);
+            decodedMessage = bsm;
+            
+         } else {
+            throw new IOException("Unknown message ID extracted: " + mf.messageId);
+         }
+      }
 
       // TODO - convert the tim message
-      tim = new J2735TravelerInformationMessage();
+      odeTim = new J2735TravelerInformationMessage();
 
-      if (tim != null) {
+      if (odeTim != null) {
          logger.debug("Decoded TIM successfully, creating OdeTimData object.");
 
-         OdeMsgPayload timDataPayload = new OdeMsgPayload(tim);
+         OdeMsgPayload timDataPayload = new OdeMsgPayload(odeTim);
          OdeMsgMetadata timMetadata = new OdeMsgMetadata(timDataPayload); // TODO
                                                                           // create
                                                                           // and
