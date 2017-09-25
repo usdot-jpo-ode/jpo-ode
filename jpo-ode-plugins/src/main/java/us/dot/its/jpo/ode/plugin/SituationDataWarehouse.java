b00fb082412c75813b30c99033322d9044906a80
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-09-25 11:23:43 -0400

    fixed ttl issue & added documentation

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/SituationDataWarehouse.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/SituationDataWarehouse.java
@@ -1,5 +1,7 @@
 package us.dot.its.jpo.ode.plugin;
 
+import com.google.gson.annotations.SerializedName;
+
 import us.dot.its.jpo.ode.model.OdeObject;
 import us.dot.its.jpo.ode.plugin.j2735.J2735GeoRegion;
 
@@ -9,11 +11,22 @@ public class SituationDataWarehouse {
       private static final long serialVersionUID = -7731139391317960325L;
 
       public enum TimeToLive {
-         ONEMINUTE, THIRTYMINUTES, ONEDAY, ONEWEEK, ONEMONTH, ONEYEAR
+         @SerializedName("oneminute")
+         ONEMINUTE, 
+         @SerializedName("thirtyminutes")
+         THIRTYMINUTES, 
+         @SerializedName("oneday")
+         ONEDAY, 
+         @SerializedName("oneweek")
+         ONEWEEK, 
+         @SerializedName("onemonth")
+         ONEMONTH, 
+         @SerializedName("oneyear")
+         ONEYEAR
       }
 
       private J2735GeoRegion serviceRegion;
-      private TimeToLive ttl = TimeToLive.THIRTYMINUTES;
+      private TimeToLive ttl = null;
 
       public J2735GeoRegion getServiceRegion() {
          return serviceRegion;
@@ -24,6 +37,7 @@ public class SituationDataWarehouse {
       }
 
       public TimeToLive getTtl() {
+         if (ttl == null) return TimeToLive.THIRTYMINUTES;
          return ttl;
       }
 
