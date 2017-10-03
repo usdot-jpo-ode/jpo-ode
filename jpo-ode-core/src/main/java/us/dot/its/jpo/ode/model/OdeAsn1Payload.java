f9881ac73f19828ee132a6894605d31c2c5eb6b5
572682 <musavi_hamid@bah.com>
2017-10-03 15:39:20 -0400

    ODE-559 Attempting to fix the XML serialization issue.

--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeAsn1Payload.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeAsn1Payload.java
@@ -6,7 +6,19 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class OdeAsn1Payload extends OdeMsgPayload {
    private static final long serialVersionUID = -7540671640610460741L;
 
-    public OdeAsn1Payload(OdeHexByteArray bytes) {
+    public OdeAsn1Payload() {
+      super();
+   }
+
+   public OdeAsn1Payload(OdeObject data) {
+      super(data);
+   }
+
+   public OdeAsn1Payload(String dataType, OdeObject data) {
+      super(dataType, data);
+   }
+
+   public OdeAsn1Payload(OdeHexByteArray bytes) {
         super(bytes);
         this.setData(bytes);
     }
