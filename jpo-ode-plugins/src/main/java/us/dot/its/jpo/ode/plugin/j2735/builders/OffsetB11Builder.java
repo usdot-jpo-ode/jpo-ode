5711a53e073850395125eb3a6f8b253472e40d94
hmusavi <hmusavi@users.noreply.github.com>
2017-10-07 17:03:48 -0400

    ODE-559 Added remaining builder classes

---
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetB11Builder.java
@@ -0,0 +1,16 @@
+package us.dot.its.jpo.ode.plugin.j2735.oss;
+
+import java.math.BigDecimal;
+
+import us.dot.its.jpo.ode.j2735.dsrc.Offset_B11;
+
+public class OssOffsetB11 {
+
+   private OssOffsetB11() {
+      throw new UnsupportedOperationException();
+   }
+
+   public static Offset_B11 offsetB11(BigDecimal offset) {
+      return new Offset_B11(offset.scaleByPowerOfTen(2).intValue());
+   }
+}
