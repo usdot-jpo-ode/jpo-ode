5aa79e0144456b6d011b4defca43d72a1aca3c4e
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-10-19 18:15:03 -0400

    ODE-587 working except for advisory bug

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetLLB16Builder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetLLB16Builder.java
@@ -2,16 +2,13 @@ package us.dot.its.jpo.ode.plugin.j2735.builders;
 
 import java.math.BigDecimal;
 
-import us.dot.its.jpo.ode.j2735.dsrc.OffsetLL_B16;
-
 public class OffsetLLB16Builder {
 
    private OffsetLLB16Builder() {
       throw new UnsupportedOperationException();
    }
 
-   public static OffsetLL_B16 offsetLLB16(BigDecimal offset) {
-      return new OffsetLL_B16(offset.scaleByPowerOfTen(7).intValue());
+   public static int offsetLLB16(BigDecimal offset) {
+      return offset.scaleByPowerOfTen(7).intValue();
    }
-
 }
--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetLLB24Builder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetLLB24Builder.java
@@ -2,16 +2,13 @@ package us.dot.its.jpo.ode.plugin.j2735.builders;
 
 import java.math.BigDecimal;
 
-import us.dot.its.jpo.ode.j2735.dsrc.OffsetLL_B24;
-
 public class OffsetLLB24Builder {
 
    private OffsetLLB24Builder() {
       throw new UnsupportedOperationException();
    }
 
-   public static OffsetLL_B24 offsetLLB24(BigDecimal offset) {
-      return new OffsetLL_B24(offset.scaleByPowerOfTen(7).intValue());
+   public static int offsetLLB24(BigDecimal offset) {
+      return offset.scaleByPowerOfTen(7).intValue();
    }
-
 }
