3f8ac81faf92f8c570cd6e106fb0b0e54562716a
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-11-02 12:13:16 -0400

    weather report unit tests

---
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/builders/VelocityBuilderTest.java
@@ -0,0 +1,48 @@
+package us.dot.its.jpo.ode.plugin.j2735.builders;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+public class VelocityBuilderTest {
+
+   @Test
+   public void testLowerBound() {
+      
+      BigDecimal testInput = BigDecimal.ZERO;
+      int expectedValue = 0;
+      assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+   @Test
+   public void testLowerBoundPlus1() {
+      
+      BigDecimal testInput = BigDecimal.valueOf(0.02);
+      int expectedValue = 1;
+      assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+   @Test
+   public void testUpperBoundMinus1() {
+      BigDecimal testInput = BigDecimal.valueOf(163.78);
+      int expectedValue = 8189;
+      assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+   @Test
+   public void testUpperBound() {
+      BigDecimal testInput = BigDecimal.valueOf(163.80);
+      int expectedValue = 8190;
+      assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+   @Test
+   public void testNullFlagValue() {
+      BigDecimal testInput = null;
+      int expectedValue = 8191;
+      assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+}
