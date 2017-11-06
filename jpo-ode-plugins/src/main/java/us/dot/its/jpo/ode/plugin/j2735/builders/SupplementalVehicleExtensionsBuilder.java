9492cb4226ca2ec65d4563e905d7fcb96ec8c385
Matthew Schwartz <schwartz_matthew2@bah.com>
2017-11-06 15:51:46 -0500

    BsmPart2ContentBuilderTest, SupplementalVehicleExtensionsBuilderTest, VehicleSafetyExtensionsBuilderTest

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/SupplementalVehicleExtensionsBuilder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/SupplementalVehicleExtensionsBuilder.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import us.dot.its.jpo.ode.plugin.j2735.J2735BsmPart2Content;
 import us.dot.its.jpo.ode.plugin.j2735.J2735RegionalContent;
 import us.dot.its.jpo.ode.plugin.j2735.J2735SupplementalVehicleExtensions;
 import us.dot.its.jpo.ode.util.CodecUtils;
@@ -14,9 +15,10 @@ public class SupplementalVehicleExtensionsBuilder {
        throw new UnsupportedOperationException();
     }
 
-    public static J2735SupplementalVehicleExtensions genericSupplementalVehicleExtensions(
+    public static J2735SupplementalVehicleExtensions genericSupplementalVehicleExtensions(J2735BsmPart2Content part2Content, 
             JsonNode sve) {
         J2735SupplementalVehicleExtensions genericSVE = new J2735SupplementalVehicleExtensions();
+        part2Content.setValue(genericSVE);
 
         // All elements of this class are optional
         if (sve.has("classification")) {
