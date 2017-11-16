b721c43ac569c8ed46f864bdd9d39b7fa9f653ec
572682 <musavi_hamid@bah.com>
2017-11-16 12:52:36 -0500

    Renamed Regions to Region

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/timstorage/Region.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/timstorage/Region.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import us.dot.its.jpo.ode.plugin.asn1.Asn1Object;
 
-public class Regions extends Asn1Object
+public class Region extends Asn1Object
 {
    private static final long serialVersionUID = 1L;
    
