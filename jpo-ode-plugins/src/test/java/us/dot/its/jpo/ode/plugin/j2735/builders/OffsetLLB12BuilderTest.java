b82180db3bfc35ff0f2d5fa7ec1779b3f367337f
levesque1 <levesque_paul@bah.com>
2017-11-16 16:53:06 -0500

    merge resolve

---
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/builders/OffsetLLB12BuilderTest.java
@@ -0,0 +1,37 @@
+package us.dot.its.jpo.ode.plugin.j2735.builders;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+public class OffsetLLB12BuilderTest {
+
+   @Test
+   public void testConversion() {
+      int expectedValue = 1234;
+      BigDecimal testInput = BigDecimal.valueOf(0.000123456);
+
+      assertEquals(expectedValue, OffsetLLB12Builder.offsetLLB12(testInput).intValue());
+   }
+
+   @Test
+   public void testConstructorIsPrivate()
+         throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+      Constructor<OffsetLLB12Builder> constructor = OffsetLLB12Builder.class.getDeclaredConstructor();
+      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+      constructor.setAccessible(true);
+      try {
+         constructor.newInstance();
+         fail("Expected IllegalAccessException.class");
+      } catch (Exception e) {
+         assertEquals(InvocationTargetException.class, e.getClass());
+      }
+   }
+}
