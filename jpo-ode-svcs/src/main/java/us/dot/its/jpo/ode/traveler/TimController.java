113ec05c0950c2b18e9d66dcfae14ca3003d4981
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-01-09 08:37:14 -0500

    split tim query into 2, re-removed the old query

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/traveler/TimController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/traveler/TimController.java
@@ -2,13 +2,7 @@ package us.dot.its.jpo.ode.traveler;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -87,8 +81,6 @@ public class TimController {
    private MessageProducer<String, String> stringMsgProducer;
    private MessageProducer<String, OdeObject> timProducer;
 
-   private ExecutorService threadPool;
-
    @Autowired
    public TimController(OdeProperties odeProperties) {
       super();
@@ -98,77 +90,8 @@ public class TimController {
             odeProperties.getKafkaProducerType());
       this.timProducer = new MessageProducer<>(odeProperties.getKafkaBrokers(), odeProperties.getKafkaProducerType(),
             null, OdeTimSerializer.class.getName());
-      
-      this.threadPool = Executors.newFixedThreadPool(odeProperties.getRsuSrmSlots() * 3);
    }
 
-   /**
-    * TODO - This is the asynchronous, slower method for querying RSUs
-    * 
-    * Checks given RSU for all TIMs set
-    * 
-    * @param jsonString
-    *           Request body containing RSU info
-    * @return list of occupied TIM slots on RSU
-    */
-   @Deprecated
-   @ResponseBody
-   @CrossOrigin
-   @RequestMapping(value = "/tim/queryold", method = RequestMethod.POST)
-   public synchronized ResponseEntity<String> asyncQueryForTims(@RequestBody String jsonString) { // NOSONAR
-
-      if (null == jsonString || jsonString.isEmpty()) {
-         logger.error("Empty request.");
-         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jsonKeyValue(ERRSTR, "Empty request."));
-      }
-
-      ConcurrentSkipListMap<Integer, Integer> resultTable = new ConcurrentSkipListMap<>();
-      RSU queryTarget = (RSU) JsonUtils.fromJson(jsonString, RSU.class);
-
-      SnmpSession snmpSession = null;
-      try {
-         snmpSession = new SnmpSession(queryTarget);
-         snmpSession.startListen();
-      } catch (IOException e) {
-         logger.error("Error creating SNMP session.", e);
-         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-               .body(jsonKeyValue(ERRSTR, "Failed to create SNMP session."));
-      }
-
-      // Repeatedly query the RSU to establish set rows
-      List<Callable<Object>> queryThreadList = new ArrayList<>();
-
-      for (int i = 0; i < odeProperties.getRsuSrmSlots(); i++) {
-         ScopedPDU pdu = new ScopedPDU();
-         pdu.add(new VariableBinding(new OID("1.0.15628.4.1.4.1.11.".concat(Integer.toString(i)))));
-         pdu.setType(PDU.GET);
-         queryThreadList.add(Executors
-               .callable(new TimQueryThread(snmpSession.getSnmp(), pdu, snmpSession.getTarget(), resultTable, i)));
-      }
-
-      try {
-         threadPool.invokeAll(queryThreadList);
-      } catch (InterruptedException e) { // NOSONAR
-         logger.error("Error submitting query threads for execution.", e);
-         threadPool.shutdownNow();
-      }
-
-      try {
-         snmpSession.endSession();
-      } catch (IOException e) {
-         logger.error("Error closing SNMP session.", e);
-      }
-
-      if (resultTable.containsValue(TimQueryThread.TIMEOUT_FLAG)) {
-         logger.error("TIM query timed out.");
-         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-               .body(jsonKeyValue(ERRSTR, "Query timeout, increase retries."));
-      } else {
-         logger.info("TIM query successful: {}", resultTable.keySet());
-         return ResponseEntity.status(HttpStatus.OK).body("{\"indicies_set\":" + resultTable.keySet() + "}");
-      }
-   }
-   
    /**
     * Checks given RSU for all TIMs set
     * 
@@ -198,41 +121,56 @@ public class TimController {
                .body(jsonKeyValue(ERRSTR, "Failed to create SNMP session."));
       }
 
-      PDU pdu = new ScopedPDU();
-      pdu.setType(PDU.GET);
+      PDU pdu0 = new ScopedPDU();
+      pdu0.setType(PDU.GET);
+      PDU pdu1 = new ScopedPDU();
+      pdu1.setType(PDU.GET);
       
-      for (int i = 0; i < odeProperties.getRsuSrmSlots(); i++) {
-         pdu.add(new VariableBinding(new OID("1.0.15628.4.1.4.1.11.".concat(Integer.toString(i)))));
+      for (int i = 0; i < odeProperties.getRsuSrmSlots()-50; i++) {
+         pdu0.add(new VariableBinding(new OID("1.0.15628.4.1.4.1.11.".concat(Integer.toString(i)))));
+      }
+      
+      for (int i = 50; i < odeProperties.getRsuSrmSlots(); i++) {
+         pdu1.add(new VariableBinding(new OID("1.0.15628.4.1.4.1.11.".concat(Integer.toString(i)))));
       }
 
-      ResponseEvent response = null;
+      ResponseEvent response0 = null;
+      ResponseEvent response1 = null;
       try {
-         response = snmpSession.getSnmp().send(pdu, snmpSession.getTarget());
+         response0 = snmpSession.getSnmp().send(pdu0, snmpSession.getTarget());
+         response1 = snmpSession.getSnmp().send(pdu1, snmpSession.getTarget());
       } catch (IOException e) {
          logger.error("Error creating SNMP session.", e);
          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                .body(jsonKeyValue(ERRSTR, "Failed to create SNMP session."));
       }
 
-      try {
-         snmpSession.endSession();
-      } catch (IOException e) {
-         logger.error("Error closing SNMP session.", e);
-      }
-
       // Process response
-      if (response == null || response.getResponse() == null) {
+      if (response0 == null || response0.getResponse() == null || response1 == null || response1.getResponse() == null) {
          logger.error("RSU query failed, timeout.");
          return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                .body(jsonKeyValue(ERRSTR, "Timeout, no response from RSU."));
       }
 
       HashMap<String, Boolean> resultsMap = new HashMap<>();
-      for (Object vbo : response.getResponse().getVariableBindings().toArray()) {
+      for (Object vbo : response0.getResponse().getVariableBindings().toArray()) {
          VariableBinding vb = (VariableBinding) vbo;
          if (vb.getVariable().toInt() == 1) {
             resultsMap.put(vb.getOid().toString().substring(21), true);
          }
+      }
+      
+      for (Object vbo : response1.getResponse().getVariableBindings().toArray()) {
+         VariableBinding vb = (VariableBinding) vbo;
+         if (vb.getVariable().toInt() == 1) {
+            resultsMap.put(vb.getOid().toString().substring(21), true);
+         }
+      }
+      
+      try {
+         snmpSession.endSession();
+      } catch (IOException e) {
+         logger.error("Error closing SNMP session.", e);
       }
 
       logger.info("RSU query successful: {}", resultsMap.keySet());
