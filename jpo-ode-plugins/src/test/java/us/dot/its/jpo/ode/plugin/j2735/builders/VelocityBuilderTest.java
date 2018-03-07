2cde7bd883677066059a5fc12f514688b9c11247
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-03-07 10:02:23 -0500

    Unit tests

--- jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/builders/VelocityBuilderTest.java
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/builders/VelocityBuilderTest.java
@@ -2,6 +2,9 @@ package us.dot.its.jpo.ode.plugin.j2735.builders;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 
 import org.junit.Test;
@@ -43,6 +46,20 @@ public class VelocityBuilderTest {
       BigDecimal testInput = null;
       int expectedValue = 8191;
       assertEquals(expectedValue, VelocityBuilder.velocity(testInput));
+   }
+   
+   @Test
+   public void testConstructorIsPrivate()
+         throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+      Constructor<VelocityBuilder> constructor = VelocityBuilder.class.getDeclaredConstructor();
+      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+      constructor.setAccessible(true);
+      try {
+         constructor.newInstance();
+         fail("Expected IllegalAccessException.class");
+      } catch (Exception e) {
+         assertEquals(InvocationTargetException.class, e.getClass());
+      }
    }
    
 }
