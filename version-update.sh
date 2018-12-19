78aadb7bff67d9a1421631b755e82fb56108f51c
Hamid Musavi <musavi_hamid@bah.com>
2018-12-19 15:55:46 -0500

    Updated version-update.sh

--- version-update.sh
+++ version-update.sh
@@ -1,34 +1,49 @@
 #!/bin/sh
-# usage: ./version-update.sh <newVersion> [<remote>]
-echo $#
-if [ $# -eq 0 ]
+# usage: ./version-update.sh <currentVersion> <nextVersion> [<remote>]
+if [ $# -lt 2 ]
   then
-    echo usage: "./version-update.sh <newVersion> [<remote>]"
+    echo usage: "./version-update.sh <currentVersion> <nextVersion> [<remote>]"
+	echo 
+    echo        "<currentVersion> and <nextVersion> are numeric version spec only. DO NOT add -SNAPSHOT"
+    echo        "For example: version-update.sh 1.0.1 1.0.2"
 	exit -1
 fi
 
-if [ "_$2_" = "__" ]
+if [ "_$3_" = "__" ]
   then
     remote=origin
   else
     remote=$2
 fi
 
-newVersion=$1
-echo updating to version $newVersion for remote $remote
+currentVersion=$1
+echo updating to from $currentVersion to $nextVersion for remote $remote
 
 #Checkout and fetch the `dev` branch
 git checkout -B dev $remote/dev
 git fetch --recurse-submodules=yes
 
 #Set the new version which should basically be removing -SNAPSHOT from `dev` branch
-mvn versions:set -DnewVersion=$newVersion
+mvn versions:set -DnewVersion=$currentVersion
 git add --update .
-git commit -m "updated to version $newVersion"
+git commit -m "updated to version $currentVersion"
 
 #Checkout and fetch the `stage` branch
-#git checkout -B stage stage
-#git fetch --recurse-submodules=yes
+git checkout -B stage $remote/stage
+git fetch --recurse-submodules=yes
 
 #Merge `dev` to `stage`
-#git merge --commit -m "merged `dev` to `stage`" dev
+git merge --commit -m "merged `dev` to `stage`" dev
+
+#Checkout and fetch the `dev` branch
+git checkout -B dev
+git fetch --recurse-submodules=yes
+
+#Set the new SNAPSHOT version
+mvn versions:set -DnewVersion=$nextVersion-SNAPSHOT
+git add --update .
+git commit -m "updated to version $nextVersion"
+
+#Push to SCM
+#git push dev
+#git push stage
