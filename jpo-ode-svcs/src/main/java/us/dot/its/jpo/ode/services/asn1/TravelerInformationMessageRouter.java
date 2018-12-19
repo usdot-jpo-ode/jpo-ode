867d54356f2bc1afb5e6409a1333f134ee566b04
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-12-19 12:26:14 -0500

    Commit for comparison

---
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/services/asn1/TravelerInformationMessageRouter.java
@@ -0,0 +1,13 @@
+package us.dot.its.jpo.ode.services.asn1;
+
+import us.dot.its.jpo.ode.wrapper.AbstractSubscriberProcessor;
+
+public class TravelerInformationMessageRouter extends AbstractSubscriberProcessor<String, String> {
+
+   @Override
+   protected Object process(String consumedData) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
