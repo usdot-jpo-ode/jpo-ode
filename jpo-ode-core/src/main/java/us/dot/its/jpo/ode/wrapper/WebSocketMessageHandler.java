4b03db7e0e2d9d5d6f517ecf32491a45cd412420
Hamid Musavi <musavi_hamid@bah.com>
2018-12-21 10:58:30 -0500

    ODE-270 Added Copyright headers and license files

--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/WebSocketMessageHandler.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/WebSocketMessageHandler.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2015 US DOT - Joint Program Office
- *
- * The Government has unlimited rights to all documents/material produced under 
- * this task order. All documents and materials, to include the source code of 
- * any software produced under this contract, shall be Government owned and the 
- * property of the Government with all rights and privileges of ownership/copyright 
- * belonging exclusively to the Government. These documents and materials may 
- * not be used or sold by the Contractor without written permission from the CO.
- * All materials supplied to the Government shall be the sole property of the 
- * Government and may not be used for any other purpose. This right does not 
- * abrogate any other Government rights.
- *
- * Contributors:
- *     Booz | Allen | Hamilton - initial API and implementation
- *******************************************************************************/
+ * Copyright 2018 572682
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package us.dot.its.jpo.ode.wrapper;
 
 import javax.websocket.CloseReason;
