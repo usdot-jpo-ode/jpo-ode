fcecb1970c970e194b08364bbf29a9a83a4eb923
Hamid Musavi <musavi_hamid@bah.com>
2019-01-02 11:27:52 -0500

    ODE-971 fixed Demo UI. StompController is WebSocket controller not a RestController.

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/stomp/StompController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/stomp/StompController.java
@@ -19,12 +19,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@RestController
+@Controller
 public class StompController {
 
     @Autowired
@@ -38,6 +39,7 @@ public class StompController {
     }
 
     @PostMapping(value="/newMessage")
+    @ResponseBody
     public String messages() {
         template.convertAndSend("/topic/messages", new StompContent("test"));
         return "{\"success\": true}" ;
