4b9daebbe16f1f9cf78d733445f96a61d1fc169f
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-03-14 16:47:50 -0400

    Add functionality for file as command line arg + general improvements

--- validation-script/main.py
+++ validation-script/main.py
@@ -7,54 +7,63 @@ import os
 from kafka import KafkaConsumer
 import unittest
 import validator as MetadataValidator
+from argparse import ArgumentParser
 
 DOCKER_HOST_IP=os.getenv('DOCKER_HOST_IP')
 assert DOCKER_HOST_IP != None, "Failed to get DOCKER_HOST_IP from environment variable"
 
-TEST_DATA = '{"metadata":{"bsmSource":"RV","securityResultCode":"success","recordGeneratedBy":"TMC_VIA_SNMP","receivedMessageDetails":{"locationData":{"elevation":1818.7,"heading":0,"latitude":41.1553613,"speed":0.02,"longitude":-104.6599478},"rxSource":"SNMP"},"schemaVersion":6,"payloadType":"us.dot.its.jpo.ode.model.OdeTimPayload","serialId":{"recordId":254,"serialNumber":1644,"streamId":"8f02ae49-da48-4a8f-b21d-68bd1da649d1","bundleSize":393,"bundleId":8},"sanitized":false,"recordGeneratedAt":"2018-12-05T20:23:41.297Z","recordType":"rxMsg","logFileName":"rxMsg_BSM&TIM.gz","odeReceivedAt":"2019-03-11T13:51:42.361Z"},"payload":{"data":{"MessageFrame":{"messageId":31,"value":{"TravelerInformation":{"timeStamp":440100,"packetID":300000000000000015,"urlB":null,"dataFrames":{"TravelerDataFrame":{"regions":{"GeographicalPath":{"closedPath":{"false":""},"anchor":{"lat":263041699,"long":-801463230},"name":"sirius_sirius_1_2_SAT-7EFD0225","laneWidth":15000,"directionality":{"both":""},"description":{"path":{"offset":{"xy":{"nodes":{"NodeXY":[{"delta":{"node-LatLon":{"lon":-801458685,"lat":263041564}}},{"delta":{"node-LatLon":{"lon":-801443410,"lat":263041226}}}]}}},"scale":0}},"id":{"id":0,"region":0},"direction":1111111111111111}},"duratonTime":32000,"sspMsgRights1":1,"sspMsgRights2":1,"startYear":2018,"msgId":{"roadSignID":{"viewAngle":1111111111111111,"mutcdCode":{"warning":""},"position":{"lat":263041699,"long":-801463230}}},"priority":5,"content":{"advisory":{"SEQUENCE":{"item":{"itis":7169}}}},"url":null,"sspTimRights":1,"sspLocationRights":1,"frameType":{"advisory":""},"startTime":440100}},"msgCnt":1}}}},"dataType":"TravelerInformation"}}'
-
-# fileType(s): bsmlog, obulog
 def upload_file(filepath):
     destination_url = 'http://' + DOCKER_HOST_IP + ':8080/upload/bsmlog'
     with open(filepath, 'rb') as file:
-        return requests.post(destination_url, files={'name':'file', 'file':file})
-
+        return requests.post(destination_url, files={'name':'file', 'file':file}, timeout=2)
 
 # listens to Kafka
-def listenToKafkaTopic(topic, msgQueue, expectedMsgCount):
-   consumer=KafkaConsumer(topic, bootstrap_servers=DOCKER_HOST_IP+':9092')
+def listenToKafkaTopic(topic, errored):
+   consumer=KafkaConsumer(topic, bootstrap_servers=DOCKER_HOST_IP+':9092', consumer_timeout_ms=7000)
    msgsReceived=0
    for msg in consumer:
-       msgsReceived += 1
-       MetadataValidator.validate_metadata(json.loads(msg.value)['metadata'])
-       msgQueue.put(msg)
-       if msgsReceived >= expectedMsgCount:
-          print("======")
-          print("[SUCCESS] All validation tests have passed.")
-          print("======")
-          return
+       MetadataValidator.validate_metadata(json.loads(msg.value)['metadata'], errored)
+
+def self_test():
+    self_test_data = '{"metadata":{"securityResultCode":"success","recordGeneratedBy":"TMC_VIA_SNMP","receivedMessageDetails":{"locationData":{"elevation":1818.7,"heading":0,"latitude":41.1553613,"speed":0.02,"longitude":-104.6599478},"rxSource":"SNMP"},"schemaVersion":6,"payloadType":"us.dot.its.jpo.ode.model.OdeTimPayload","serialId":{"recordId":254,"serialNumber":1644,"streamId":"8f02ae49-da48-4a8f-b21d-68bd1da649d1","bundleSize":393,"bundleId":8},"sanitized":false,"recordGeneratedAt":"2018-12-05T20:23:41.297Z","recordType":"rxMsg","logFileName":"rxMsg_BSM&TIM.gz","odeReceivedAt":"2019-03-11T13:51:42.361Z"},"payload":{"data":{"MessageFrame":{"messageId":31,"value":{"TravelerInformation":{"timeStamp":440100,"packetID":300000000000000015,"urlB":null,"dataFrames":{"TravelerDataFrame":{"regions":{"GeographicalPath":{"closedPath":{"false":""},"anchor":{"lat":263041699,"long":-801463230},"name":"sirius_sirius_1_2_SAT-7EFD0225","laneWidth":15000,"directionality":{"both":""},"description":{"path":{"offset":{"xy":{"nodes":{"NodeXY":[{"delta":{"node-LatLon":{"lon":-801458685,"lat":263041564}}},{"delta":{"node-LatLon":{"lon":-801443410,"lat":263041226}}}]}}},"scale":0}},"id":{"id":0,"region":0},"direction":1111111111111111}},"duratonTime":32000,"sspMsgRights1":1,"sspMsgRights2":1,"startYear":2018,"msgId":{"roadSignID":{"viewAngle":1111111111111111,"mutcdCode":{"warning":""},"position":{"lat":263041699,"long":-801463230}}},"priority":5,"content":{"advisory":{"SEQUENCE":{"item":{"itis":7169}}}},"url":null,"sspTimRights":1,"sspLocationRights":1,"frameType":{"advisory":""},"startTime":440100}},"msgCnt":1}}}},"dataType":"TravelerInformation"}}'
+    print("[INFO] Running self-test of validation routine...")
+    MetadataValidator.validate_metadata(json.loads(self_test_data)['metadata'])
+    print("[INFO] Self test passed.")
 
 # main
 def main():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filepath", help="Path to file used for testing.", metavar="FILE", required=True)
+    args = parser.parse_args()
+
+    # local test
+    # self_test()
+
     # Create a kafka consumer and wait for it to connect
-    print("======")
-    print("Preparing...")
+    print("[INFO] Preparing Kafka listener...")
     topic_name='topic.OdeBsmJson'
     msg_queue = queue.Queue()
-    kafkaListenerThread=threading.Thread(target=listenToKafkaTopic,args=(topic_name, msg_queue, 222,))
+    errored = False
+    kafkaListenerThread=threading.Thread(target=listenToKafkaTopic,args=(topic_name, msg_queue,))
     kafkaListenerThread.start()
-    time.sleep(5)
-    print("Preparation complete.")
-    print("======")
-    print("Uploading test file to ODE...")
-    upload_response = upload_file('./data/bsmLogDuringEvent.gz')
+    time.sleep(3)
+    print("[INFO] Kafka listener preparation complete.")
+
+    print("[INFO] Uploading test file to ODE...")
+    upload_response = upload_file(args.filepath)
     if upload_response.status_code == 200:
-        print("Test file uploaded successfully.")
+        print("[INFO] Test file uploaded successfully.")
     else:
-        print("Error, aborting test routine! Test file failed to upload, response code %d" % upload_response.status_code)
+        print("[ERROR] Aborting test routine! Test file failed to upload, response code %d" % upload_response.status_code)
         raise SystemExit
-    print("======")
-    print("Starting metadata validation...")
+
+    print("[INFO] Starting metadata validation...")
+    kafkaListenerThread.join()
+    print("[INFO] Metadata validation complete.")
+    if msg_queue.empty():
+        print("[SUCCESS] All validation tests have passed.")
+    else:
+        print("[FAILED] Message file '%s' has failed validation." % args.filepath)
 
 if __name__ == '__main__':
     main()
--- validation-script/validator.py
+++ validation-script/validator.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 import dateutil.parser
+import os
 
 ##################################
 ### Expected Fields
@@ -48,6 +49,7 @@ SPEED_LOWER_LIMIT = Decimal(0)
 HEADING_UPPER_LIMIT = Decimal(359.9875)
 HEADING_LOWER_LIMIT = Decimal(0)
 BSM_SOURCE_LIST = ['RV', 'EV', 'unknown']
+# BSM_SOURCE_LIST = ['bob','alice']
 RX_SOURCE_LIST = ['RSU', 'SAT', 'RV', 'SNMP', 'NA', 'unknown']
 RECORD_GENERATED_BY_LIST = ['TMC', 'OBU', 'RSU', 'TMC_VIA_SAT', 'TMC_VIA_SNMP']
 RECORD_TYPE_LIST = ['bsmLogDuringEvent', 'rxMsg', 'dnMsg', 'bsmTx', 'driverAlert', 'unsupported']
@@ -64,60 +66,62 @@ SERIAL_NUMBER_UPPER_LIMIT = 9223372036854775807
 SERIAL_NUMBER_LOWER_LIMIT = 0
 
 # validates all fields of the metadata
-def validate_metadata(metadata):
-    validate_field('bsmSource', metadata.get('bsmSource'), enum_list=BSM_SOURCE_LIST)
-    validate_field('logFileName', metadata.get('logFileName'))
-    validate_field('odeReceivedAt', metadata.get('odeReceivedAt'))
-    validate_field('payloadType', metadata.get('payloadType'))
-    validate_field('receivedMessageDetails', metadata.get('receivedMessageDetails'))
-    validate_field('locationData', metadata['receivedMessageDetails'].get('locationData'))
-    validate_field('latitude', metadata['receivedMessageDetails']['locationData'].get('latitude'), upper_limit=LATITUDE_UPPER_LIMIT, lower_limit=LATITUDE_LOWER_LIMIT)
-    validate_field('longitude', metadata['receivedMessageDetails']['locationData'].get('longitude'), upper_limit=LONGITUDE_UPPER_LIMIT, lower_limit=LONGITUDE_LOWER_LIMIT)
-    validate_field('elevation', metadata['receivedMessageDetails']['locationData'].get('elevation'), upper_limit=ELEVATION_UPPER_LIMIT, lower_limit=ELEVATION_LOWER_LIMIT)
-    validate_field('speed', metadata['receivedMessageDetails']['locationData'].get('speed'), upper_limit=SPEED_UPPER_LIMIT, lower_limit=SPEED_LOWER_LIMIT)
-    validate_field('heading', metadata['receivedMessageDetails']['locationData'].get('heading'), upper_limit=HEADING_UPPER_LIMIT, lower_limit=HEADING_LOWER_LIMIT)
-    validate_field('rxSource', metadata['receivedMessageDetails'].get('rxSource'), enum_list=RX_SOURCE_LIST)
-    validate_field('recordGeneratedAt', metadata.get('recordGeneratedAt'), timestamp=True)
-    validate_field('recordGeneratedBy', metadata.get('recordGeneratedBy'), enum_list=RECORD_GENERATED_BY_LIST)
-    validate_field('recordType', metadata.get('recordType'), enum_list=RECORD_TYPE_LIST)
-    validate_field('sanitized', metadata.get('sanitized'), enum_list=SANITIZED_LIST)
-    validate_field('schemaVersion', metadata.get('schemaVersion'), equals_value=SCHEMA_VERSION)
-    validate_field('securityResultCode', metadata.get('securityResultCode'), enum_list=SECURITY_RESULT_CODE_LIST)
-    validate_field('serialId', metadata.get('serialId'))
-    validate_field('streamId', metadata['serialId'].get('streamId'))
-    validate_field('bundleSize', metadata['serialId'].get('bundleSize'), upper_limit=BUNDLE_SIZE_UPPER_LIMIT, lower_limit=BUNDLE_SIZE_LOWER_LIMIT)
-    validate_field('bundleId', metadata['serialId'].get('bundleId'), upper_limit=BUNDLE_ID_UPPER_LIMIT, lower_limit=BUNDLE_ID_LOWER_LIMIT)
-    validate_field('recordId', metadata['serialId'].get('recordId'), upper_limit=RECORD_ID_UPPER_LIMIT, lower_limit=RECORD_ID_LOWER_LIMIT)
-    validate_field('serialNumber', metadata['serialId'].get('serialNumber'), upper_limit=SERIAL_NUMBER_UPPER_LIMIT, lower_limit=SERIAL_NUMBER_LOWER_LIMIT)
+def validate_metadata(metadata, errored):
+    validate_field(errored, 'payloadType', metadata.get('payloadType'))
+    if metadata.get('payloadType') == "us.dot.its.jpo.ode.model.OdeBsmPayload":
+        validate_field(errored, 'bsmSource', metadata.get('bsmSource'), enum_list=BSM_SOURCE_LIST)
+    validate_field(errored, 'logFileName', metadata.get('logFileName'))
+    validate_field(errored, 'odeReceivedAt', metadata.get('odeReceivedAt'))
+    validate_field(errored, 'receivedMessageDetails', metadata.get('receivedMessageDetails'))
+    validate_field(errored, 'locationData', metadata['receivedMessageDetails'].get('locationData'))
+    validate_field(errored, 'latitude', metadata['receivedMessageDetails']['locationData'].get('latitude'), upper_limit=LATITUDE_UPPER_LIMIT, lower_limit=LATITUDE_LOWER_LIMIT)
+    validate_field(errored, 'longitude', metadata['receivedMessageDetails']['locationData'].get('longitude'), upper_limit=LONGITUDE_UPPER_LIMIT, lower_limit=LONGITUDE_LOWER_LIMIT)
+    validate_field(errored, 'elevation', metadata['receivedMessageDetails']['locationData'].get('elevation'), upper_limit=ELEVATION_UPPER_LIMIT, lower_limit=ELEVATION_LOWER_LIMIT)
+    validate_field(errored, 'speed', metadata['receivedMessageDetails']['locationData'].get('speed'), upper_limit=SPEED_UPPER_LIMIT, lower_limit=SPEED_LOWER_LIMIT)
+    validate_field(errored, 'heading', metadata['receivedMessageDetails']['locationData'].get('heading'), upper_limit=HEADING_UPPER_LIMIT, lower_limit=HEADING_LOWER_LIMIT)
+    validate_field(errored, 'rxSource', metadata['receivedMessageDetails'].get('rxSource'), enum_list=RX_SOURCE_LIST)
+    validate_field(errored, 'recordGeneratedAt', metadata.get('recordGeneratedAt'), timestamp=True)
+    validate_field(errored, 'recordGeneratedBy', metadata.get('recordGeneratedBy'), enum_list=RECORD_GENERATED_BY_LIST)
+    validate_field(errored, 'recordType', metadata.get('recordType'), enum_list=RECORD_TYPE_LIST)
+    validate_field(errored, 'sanitized', metadata.get('sanitized'), enum_list=SANITIZED_LIST)
+    validate_field(errored, 'schemaVersion', metadata.get('schemaVersion'), equals_value=SCHEMA_VERSION)
+    validate_field(errored, 'securityResultCode', metadata.get('securityResultCode'), enum_list=SECURITY_RESULT_CODE_LIST)
+    validate_field(errored, 'serialId', metadata.get('serialId'))
+    validate_field(errored, 'streamId', metadata['serialId'].get('streamId'))
+    validate_field(errored, 'bundleSize', metadata['serialId'].get('bundleSize'), upper_limit=BUNDLE_SIZE_UPPER_LIMIT, lower_limit=BUNDLE_SIZE_LOWER_LIMIT)
+    validate_field(errored, 'bundleId', metadata['serialId'].get('bundleId'), upper_limit=BUNDLE_ID_UPPER_LIMIT, lower_limit=BUNDLE_ID_LOWER_LIMIT)
+    validate_field(errored, 'recordId', metadata['serialId'].get('recordId'), upper_limit=RECORD_ID_UPPER_LIMIT, lower_limit=RECORD_ID_LOWER_LIMIT)
+    validate_field(errored, 'serialNumber', metadata['serialId'].get('serialNumber'), upper_limit=SERIAL_NUMBER_UPPER_LIMIT, lower_limit=SERIAL_NUMBER_LOWER_LIMIT)
 
 # all-in-one validation checker
-def validate_field(field_name, field_value, upper_limit=None, lower_limit=None, enum_list=None, equals_value=None, timestamp=None):
+def validate_field(errored, field_name, field_value, upper_limit=None, lower_limit=None, enum_list=None, equals_value=None, timestamp=None):
     if field_value is None:
-        error(field_name, "field is missing")
+        error(errored, field_name, "field is missing")
     if field_value == "":
-        error(field_name, "field is empty")
-    if upper_limit is not None and field_value > upper_limit:
-        error(field_name, "field value %s is greater than upper limit: %s" % (str(field_value), str(upper_limit)))
-    if lower_limit is not None and field_value < lower_limit:
-        error(field_name, "field value %s is less than lower limit: %s" % (str(field_value), str(lower_limit)))
+        error(errored, field_name, "field is empty")
+    if upper_limit is not None and Decimal(field_value) > upper_limit:
+        error(errored, field_name, "field value %s is greater than upper limit: %s" % (str(field_value), str(upper_limit)))
+    if lower_limit is not None and Decimal(field_value) < lower_limit:
+        error(errored, field_name, "field value %s is less than lower limit: %s" % (str(field_value), str(lower_limit)))
     if enum_list is not None and field_value not in enum_list:
-        error(field_name, "field value %s not in list of known values: [%s]" % (str(field_value), ', '.join(map(str, enum_list))))
-    if equals_value is not None and field_value != equals_value:
-        error(field_name, "field value %s does not match expected value: %s" % (str(field_value), str(equals_value)))
+        error(errored, field_name, "field value %s not in list of known values: [%s]" % (str(field_value), ', '.join(map(str, enum_list))))
+    if equals_value is not None and Decimal(field_value) != equals_value:
+        error(errored, field_name, "field value %s does not match expected value: %s" % (str(field_value), str(equals_value)))
     if timestamp is not None:
-        validate_timestamp(field_name, field_value)
+        validate_timestamp(errored, field_name, field_value)
 
 # validates timestamp by parsability
-def validate_timestamp(field_name, string_time):
+def validate_timestamp(errored, field_name, string_time):
     try:
         dateutil.parser.parse(string_time)
     except Exception as e:
-        error(field_name, str(e))
+        error(errored, field_name, str(e))
 
 # prints error and exits program
-def error(field_name, err_msg):
+def error(errored, field_name, err_msg):
     print("======")
     print("[ERROR] Validation Failure!")
     print("[ERROR] Field '%s' failed validation with error: %s" % (field_name, err_msg))
     print("======")
+    errored.put(True)
     raise SystemExit
