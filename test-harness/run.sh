2d639230fa24fc0960cd4adcda153923d8bb4e1f
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-04-01 14:54:12 -0400

    Delete deprecated test-harness folder

--- test-harness/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [[ -z "$DOCKER_HOST_IP" ]]; then
-   echo "ERROR! Required environment variable DOCKER_HOST_IP not set"
-   exit 1
-fi
-docker build -t ode-test-harness .
-docker run --rm -e DOCKER_HOST_IP=$DOCKER_HOST_IP ode-test-harness:latest
