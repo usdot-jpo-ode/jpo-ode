3f3ff57c9bcff30300542254cdb236f977956774
djohnston <djohnston@neaeraconsulting.com>
2021-10-03 17:05:38 -0600

    one more enum fix

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/RequestorTypeBuilder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/RequestorTypeBuilder.java
@@ -19,21 +19,21 @@ public class RequestorTypeBuilder {
         JsonNode role = typeData.get("role");
 		if(role != null)
 		{
-            J2735BasicVehicleRole enumRole = J2735BasicVehicleRole.valueOf(role.asText());
+            J2735BasicVehicleRole enumRole = J2735BasicVehicleRole.valueOf(role.fieldNames().next());
 			requestorType.setRole(enumRole);
 		}
 
         JsonNode subrole = typeData.get("subrole");
 		if(subrole != null)
 		{
-            J2735RequestSubRole enumSubrole = J2735RequestSubRole.valueOf(subrole.asText());
+            J2735RequestSubRole enumSubrole = J2735RequestSubRole.valueOf(subrole.fieldNames().next());
 			requestorType.setSubrole(enumSubrole);
 		}
 
         JsonNode request = typeData.get("request");
 		if(request != null)
 		{
-            J2735RequestImportanceLevel enumRequest = J2735RequestImportanceLevel.valueOf(request.asText());
+            J2735RequestImportanceLevel enumRequest = J2735RequestImportanceLevel.valueOf(request.fieldNames().next());
 			requestorType.setRequest(enumRequest);
 		}
 
