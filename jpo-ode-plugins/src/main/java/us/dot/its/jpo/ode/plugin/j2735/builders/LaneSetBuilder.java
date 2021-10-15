83a425d227cb5e391fe7c74c032463dfd578d9e5
djohnston <djohnston@neaeraconsulting.com>
2021-10-14 19:22:12 -0600

    debugging lanesetbuilder

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/LaneSetBuilder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/LaneSetBuilder.java
@@ -14,17 +14,21 @@ public class LaneSetBuilder {
 	public static J2735LaneList genericLaneSet(JsonNode laneSetNode) {
 		J2735LaneList laneList = new J2735LaneList();
 
+		System.out.println(laneSetNode.toString());
+
 		if (laneSetNode.isArray()) {
 			Iterator<JsonNode> elements = laneSetNode.elements();
 
 			while (elements.hasNext()) {
+				System.out.println("Trying to make GenericLane from a list");
 				laneList.getLaneSet()
-                    .add(GenericLaneBuilder.genericGenericLane(elements.next()));
+                    .add(GenericLaneBuilder.genericGenericLane(elements.next().get("GenericLane")));
 			}
 		} else {
 			JsonNode genericLane = laneSetNode.get("GenericLane");
 			if(genericLane != null)
 			{
+				System.out.println("Trying to make GenericLane from a non-list");
 				laneList.getLaneSet()
                 	.add(GenericLaneBuilder.genericGenericLane(genericLane));
 			}
