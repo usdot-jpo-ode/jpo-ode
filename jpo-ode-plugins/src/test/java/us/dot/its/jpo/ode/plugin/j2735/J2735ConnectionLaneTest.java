c6119ee1c2130da0185c343c94eb856e13dc3061
djohnston <djohnston@neaeraconsulting.com>
2021-10-28 09:56:19 -0600

    Updated commented unit tests to work with new code

--- jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/J2735ConnectionLaneTest.java
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/J2735ConnectionLaneTest.java
@@ -10,9 +10,21 @@ public class J2735ConnectionLaneTest {
 	public void testGettersSetters() {
 		J2735ConnectingLane connectionLane = new J2735ConnectingLane();
 		connectionLane.setLane(10);
-		//connectionLane.setManeuver(J2735AllowedManeuvers.caution);
-		
 		assertEquals(connectionLane.getLane(), 10);
-		//assertEquals(connectionLane.getManeuver(), J2735AllowedManeuvers.caution);
+
+		J2735BitString maneuver = new J2735BitString();
+
+		char[] chars = "010000000000".toCharArray();
+
+		for (char i = 0; i < chars.length; i++) {
+			if (i >= J2735AllowedManeuvers.values().length) {
+				break;
+			}
+			String bitName = J2735AllowedManeuvers.values()[i].name();
+			Boolean bitStatus = (chars[i] == '1');
+			maneuver.put(bitName, bitStatus);
+		}
+		connectionLane.setManeuver(maneuver);
+		assertEquals(connectionLane.getManeuver(), maneuver);
 	}
 }
