3a276966999be9b41cc74533548bf36074dd581d
djohnston <djohnston@neaeraconsulting.com>
2022-02-22 13:56:03 -0700

    Fix TransmissionAndSpeed

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/TransmissionAndSpeedBuilder.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/builders/TransmissionAndSpeedBuilder.java
@@ -21,10 +21,6 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735TransmissionAndSpeed;
 import us.dot.its.jpo.ode.plugin.j2735.J2735TransmissionState;
 
 public class TransmissionAndSpeedBuilder {
-
-    private static final String TRANSMISSION = "transmission";
-    private static final long TRANSMISSION_LOWER_BOUND = 0L;
-    private static final long TRANSMISSION_UPPER_BOUND = 7L;
     
     private TransmissionAndSpeedBuilder() {
        throw new UnsupportedOperationException();
@@ -33,18 +29,13 @@ public class TransmissionAndSpeedBuilder {
     public static J2735TransmissionAndSpeed genericTransmissionAndSpeed(JsonNode ts) {
 
         System.out.println("TransmissionAndSpeedBuilder: Starting to configure using " + ts.toString());
-
-        if (ts.get(TRANSMISSION).asLong() < TRANSMISSION_LOWER_BOUND
-                || ts.get(TRANSMISSION).asLong() > TRANSMISSION_UPPER_BOUND) {
-            throw new IllegalArgumentException("Transmission value out of bounds [0..7]");
-        }
-
         J2735TransmissionAndSpeed gts = new J2735TransmissionAndSpeed();
 
         System.out.println("TransmissionAndSpeedBuilder: Trying to configure speed...");
         gts.setSpeed(SpeedOrVelocityBuilder.genericVelocity(ts.get("speed")));
+
         System.out.println("TransmissionAndSpeedBuilder: Trying to configure transmission...");
-        gts.setTransmisson(J2735TransmissionState.valueOf(ts.get(TRANSMISSION).asText().toUpperCase()));
+        gts.setTransmisson(J2735TransmissionState.valueOf(ts.get("transmission").fieldNames().next().toUpperCase()));
 
         return gts;
     }
