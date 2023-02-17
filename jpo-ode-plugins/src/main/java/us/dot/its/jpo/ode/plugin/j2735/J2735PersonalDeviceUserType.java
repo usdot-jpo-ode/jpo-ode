dcd785e16d011c386863bebd4a05f77562c7ce78
Michael7371 <michael@neaera.net>
2023-02-17 16:56:15 -0700

    updated enums to uppercase

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PersonalDeviceUserType.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PersonalDeviceUserType.java
@@ -1,9 +1,9 @@
 package us.dot.its.jpo.ode.plugin.j2735;
 
 public enum J2735PersonalDeviceUserType {
-    unavailable, 
-    aPEDESTRIAN, 
-    aPEDALCYCLIST, 
-    aPUBLICSAFETYWORKER, 
-    anANIMAL
+    UNAVAILABLE, 
+    APEDESTRIAN, 
+    APEDALCYCLIST, 
+    APUBLICSAFETYWORKER, 
+    ANANIMAL
 }
--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PublicSafetyEventResponderWorkerType.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/J2735PublicSafetyEventResponderWorkerType.java
@@ -1,12 +1,12 @@
 package us.dot.its.jpo.ode.plugin.j2735;
 
 public enum J2735PublicSafetyEventResponderWorkerType {
-    unavailable, 
-    towOperater, 
-    fireAndEMSWorker, 
-    aDOTWorker, 
-    lawEnforcement, 
-    hazmatResponder, 
-    animalControlWorker, 
-    otherPersonnel
+    UNAVAILABLE, 
+    TOWOPERATER, 
+    FIREANDEMSWORKER, 
+    ADOTWORKER, 
+    LAWENFORCEMENT, 
+    HAZMATRESPONDER, 
+    ANIMALCONTROLWORKER, 
+    OTHERPERSONNEL
 }
