92b00cdbe0c0e0559b0693bc737852c427dcec0f
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2024-06-21 15:56:17 -0600

    Add a startup script to the Dockerfile entry point to support a SSH/SCP server

---
+++ startup_jpoode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# NOTE: This is a bash script intended to be used by the Dockerfile and not intended to be used on its own
+
+# Create the necessary openrc directories on boot of the new container
+sh -c rc-status
+# Create the necessary file to run a service on openrc. This is necessary when using an alpine image
+touch /run/openrc/softlevel
+# Start the SSH/SCP server on boot of the new container
+rc-service sshd start
+
+# Start the jpo-ode application
+java -Djava.rmi.server.hostname=$DOCKER_HOST_IP -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dlogback.configurationFile=/home/logging/logback.xml -jar /home/jpo-ode-svcs.jar
