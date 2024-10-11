be57013143a427d5df26ae2e81d6df8096a99c45
Matt Cook <matt.cook@trihydro.com>
2024-10-11 15:44:30 -0600

    update README notification config instructions. remove template comment from test

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TimIngestWatcherTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TimIngestWatcherTest.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
-
 package us.dot.its.jpo.ode.traveler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
