bfab3254e4b103ce70e2341d1ed680ef4ba1ff62
dmccoystephenson <dmccoystephenson@gmail.com>
2024-12-02 09:19:27 -0700

    Removed unnecessary getters & setters from OdeTravelerInputData

--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeTravelerInputData.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeTravelerInputData.java
@@ -21,28 +21,9 @@ import us.dot.its.jpo.ode.plugin.j2735.OdeTravelerInformationMessage;
 
 @Data
 public class OdeTravelerInputData extends OdeObject {
-
   private static final long serialVersionUID = 8769107278440796699L;
-
   private ServiceRequest request;
-
   private OdeTravelerInformationMessage tim;
-
-  public ServiceRequest getRequest() {
-    return request;
-  }
-
-  public void setRequest(ServiceRequest request) {
-    this.request = request;
-  }
-
-  public OdeTravelerInformationMessage getTim() {
-    return tim;
-  }
-
-  public void setTim(OdeTravelerInformationMessage tim) {
-    this.tim = tim;
-  }
 
   public boolean equals(final Object o) {
     if (o == this) {
