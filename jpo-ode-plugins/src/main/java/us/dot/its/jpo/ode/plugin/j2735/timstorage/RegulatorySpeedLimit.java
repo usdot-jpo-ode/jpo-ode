08292e58ca0b5124edb597fee0795776ab72df8c
dmccoystephenson <dmccoystephenson@gmail.com>
2024-12-05 15:10:15 -0700

    Addressed checkstyle warnings for RegulatorySpeedLimit, NodeSetXY, Nodes, Anchor, DistanceUnits, NodeOffsetPointXY, DirectionOfUse, ComputedLane, Circle, ShapePointSet & MsgId

--- jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/timstorage/RegulatorySpeedLimit.java
+++ jpo-ode-plugins/src/main/java/us/dot/its/jpo/ode/plugin/j2735/timstorage/RegulatorySpeedLimit.java
@@ -3,12 +3,12 @@ package us.dot.its.jpo.ode.plugin.j2735.timstorage;
 import lombok.EqualsAndHashCode;
 import us.dot.its.jpo.ode.plugin.asn1.Asn1Object;
 
+/**
+ * The speed limit for a given section of roadway.
+ */
 @EqualsAndHashCode(callSuper = false)
 public class RegulatorySpeedLimit extends Asn1Object {
 
-  /**
-   *
-   */
   private static final long serialVersionUID = 1L;
 
   private SpeedLimitType type;
