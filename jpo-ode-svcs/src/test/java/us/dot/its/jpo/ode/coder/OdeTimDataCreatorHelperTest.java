7dccf75924d84d38273286bad6a3b731344cc769
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2024-12-09 14:10:05 -0700

    Update the imported assertEquals

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/coder/OdeTimDataCreatorHelperTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/coder/OdeTimDataCreatorHelperTest.java
@@ -1,6 +1,6 @@
 package us.dot.its.jpo.ode.coder;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.nio.file.Files;
