eb8b82d096c09ef32d21b038e5bcedb7fbedb06c
dmccoystephenson <dmccoystephenson@gmail.com>
2025-01-21 08:10:43 -0700

    Pulled JSON into separate files for J2735BsmPart2ContentTest.java
    Signed-off-by: dmccoystephenson <dmccoystephenson@gmail.com>

--- jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/J2735BsmPart2ContentTest.java
+++ jpo-ode-plugins/src/test/java/us/dot/its/jpo/ode/plugin/j2735/J2735BsmPart2ContentTest.java
@@ -4,6 +4,9 @@ import static us.dot.its.jpo.ode.plugin.j2735.J2735BsmPart2Content.J2735BsmPart2
 import static us.dot.its.jpo.ode.plugin.j2735.J2735BsmPart2Content.J2735BsmPart2Id.SupplementalVehicleExtensions;
 import static us.dot.its.jpo.ode.plugin.j2735.J2735BsmPart2Content.J2735BsmPart2Id.VehicleSafetyExtensions;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import us.dot.its.jpo.ode.util.JsonUtils;
@@ -11,10 +14,10 @@ import us.dot.its.jpo.ode.util.JsonUtils;
 class J2735BsmPart2ContentTest {
 
   @Test
-  void shouldDeserializeBsmPart2Content_VehicleSafetyExtensions() {
+  void shouldDeserializeBsmPart2Content_VehicleSafetyExtensions() throws IOException {
 
-    final String bsmJson =
-        "{\"id\":\"VehicleSafetyExtensions\",\"value\":{\"pathHistory\":{\"crumbData\":[{\"elevationOffset\":0.0,\"latOffset\":-0.0000491,\"lonOffset\":-0.0000043,\"timeOffset\":0.39},{\"elevationOffset\":-1.1,\"latOffset\":-0.0007303,\"lonOffset\":-0.0001015,\"timeOffset\":6.79},{\"elevationOffset\":-1.1,\"latOffset\":-0.0012664,\"lonOffset\":-0.0002581,\"timeOffset\":10.9},{\"elevationOffset\":-0.7,\"latOffset\":-0.0018413,\"lonOffset\":-0.0005267,\"timeOffset\":14.39}]},\"pathPrediction\":{\"confidence\":70.0,\"radiusOfCurve\":-3139.0}}}";
+    final String bsmJson = loadResourceAsString(
+        "src/test/resources/us/dot/its/jpo/ode/plugin/j2735/BSM_PartIIContent_VehicleSafetyExtensions.json");
 
     var deserialized =
         (J2735BsmPart2Content) JsonUtils.fromJson(bsmJson, J2735BsmPart2Content.class);
@@ -28,9 +31,9 @@ class J2735BsmPart2ContentTest {
   }
 
   @Test
-  void shouldDeserializeBsmPart2Content_SupplementalVehicleExtensions() {
-    final String bsmJson =
-        "{\"id\":\"SupplementalVehicleExtensions\",\"value\":{\"classification\":null,\"classDetails\":null,\"vehicleData\":null,\"doNotUse1\":null,\"doNotUse2\":null,\"doNotUse3\":null,\"status\":null,\"doNotUse4\":null,\"doNotUse5\":null}}";
+  void shouldDeserializeBsmPart2Content_SupplementalVehicleExtensions() throws IOException {
+    final String bsmJson = loadResourceAsString(
+        "src/test/resources/us/dot/its/jpo/ode/plugin/j2735/BSM_PartIIContent_SupplementalVehicleExtensions.json");
 
     var deserialized =
         (J2735BsmPart2Content) JsonUtils.fromJson(bsmJson, J2735BsmPart2Content.class);
@@ -41,9 +44,10 @@ class J2735BsmPart2ContentTest {
   }
 
   @Test
-  void shouldDeserializeBsmPart2Content_SpecialVehicleExtensions() {
-    final String bsmJson =
-        "{\"id\":\"SpecialVehicleExtensions\",\"value\":{\"vehicleAlerts\":null,\"description\":null,\"doNotUse\":null}}";
+  void shouldDeserializeBsmPart2Content_SpecialVehicleExtensions() throws IOException {
+
+    final String bsmJson = loadResourceAsString(
+        "src/test/resources/us/dot/its/jpo/ode/plugin/j2735/BSM_PartIIContent_SpecialVehicleExtensions.json");
 
     var deserialized =
         (J2735BsmPart2Content) JsonUtils.fromJson(bsmJson, J2735BsmPart2Content.class);
@@ -51,5 +55,11 @@ class J2735BsmPart2ContentTest {
     Assertions.assertNotNull(deserialized);
     Assertions.assertEquals(SpecialVehicleExtensions, deserialized.getId());
     Assertions.assertInstanceOf(J2735SpecialVehicleExtensions.class, deserialized.getValue());
+  }
+
+  static String loadResourceAsString(String resourcePath) throws IOException {
+    File file = new File(resourcePath);
+    byte[] data = Files.readAllBytes(file.toPath());
+    return new String(data);
   }
 }
