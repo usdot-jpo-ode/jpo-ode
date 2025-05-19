51764b535d868893ded420afd261a79073273e54
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-05-19 12:35:11 -0600

    adding formatting for vscode and updating checkstyle violations

--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeAsn1Payload.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeAsn1Payload.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2018 572682
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -20,48 +20,48 @@ package us.dot.its.jpo.ode.model;
  * Extends OdeMsgPayload to handle ASN.1 specific data.
  */
 public class OdeAsn1Payload extends OdeMsgPayload<OdeObject> {
-   private static final long serialVersionUID = -7540671640610460741L;
+  private static final long serialVersionUID = -7540671640610460741L;
 
-   public OdeAsn1Payload() {
-      super();
-   }
+  public OdeAsn1Payload() {
+    super();
+  }
 
-   /**
-    * Constructs an ASN.1 payload with the given ODE object data.
-    *
-    * @param data the ODE object containing ASN.1 data
-    */
-   public OdeAsn1Payload(OdeObject data) {
-      super(data);
-   }
+  /**
+   * Constructs an ASN.1 payload with the given ODE object data.
+   *
+   * @param data the ODE object containing ASN.1 data
+   */
+  public OdeAsn1Payload(OdeObject data) {
+    super(data);
+  }
 
-   /**
-    * Constructs an ASN.1 payload with specified data type and ODE object.
-    *
-    * @param dataType the type of the ASN.1 data
-    * @param data     the ODE object containing ASN.1 data
-    */
-   public OdeAsn1Payload(String dataType, OdeObject data) {
-      super(dataType, data);
-   }
+  /**
+   * Constructs an ASN.1 payload with specified data type and ODE object.
+   *
+   * @param dataType the type of the ASN.1 data
+   * @param data     the ODE object containing ASN.1 data
+   */
+  public OdeAsn1Payload(String dataType, OdeObject data) {
+    super(dataType, data);
+  }
 
-   /**
-    * Constructs an ASN.1 payload from hex-encoded byte array.
-    *
-    * @param bytes the hex-encoded byte array containing ASN.1 data
-    */
-   public OdeAsn1Payload(OdeHexByteArray bytes) {
-      super(bytes);
-      this.setData(bytes);
-   }
+  /**
+   * Constructs an ASN.1 payload from hex-encoded byte array.
+   *
+   * @param bytes the hex-encoded byte array containing ASN.1 data
+   */
+  public OdeAsn1Payload(OdeHexByteArray bytes) {
+    super(bytes);
+    this.setData(bytes);
+  }
 
-   /**
-    * Constructs an ASN.1 payload from raw bytes.
-    *
-    * @param bytes the raw byte array containing ASN.1 data
-    */
-   public OdeAsn1Payload(byte[] bytes) {
-      this(new OdeHexByteArray(bytes));
-   }
+  /**
+   * Constructs an ASN.1 payload from raw bytes.
+   *
+   * @param bytes the raw byte array containing ASN.1 data
+   */
+  public OdeAsn1Payload(byte[] bytes) {
+    this(new OdeHexByteArray(bytes));
+  }
 
 }
--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeData.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/model/OdeData.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright 2018 572682
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- *   http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2018 572682 Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
+ * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
  ******************************************************************************/
 
 package us.dot.its.jpo.ode.model;
@@ -21,19 +17,19 @@ import lombok.Setter;
 /**
  * Base class for ODE data objects containing metadata and payload.
  *
- * @param <TMetadata> the type of metadata associated with this data
- * @param <TPayload> the type of payload contained in this data
+ * @param <MetadataT> the type of metadata associated with this data
+ * @param <PayloadT> the type of payload contained in this data
  */
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
 @JsonPropertyOrder({"metadata", "payload"})
-public class OdeData<TMetadata extends OdeMsgMetadata,
-    TPayload extends OdeMsgPayload<?>> extends OdeObject implements OdeFilterable {
+public class OdeData<MetadataT extends OdeMsgMetadata, PayloadT extends OdeMsgPayload<?>>
+    extends OdeObject implements OdeFilterable {
   private static final long serialVersionUID = -7711340868799607662L;
 
-  private TMetadata metadata;
-  private TPayload payload;
+  private MetadataT metadata;
+  private PayloadT payload;
 
   public OdeData() {
     super();
@@ -45,7 +41,7 @@ public class OdeData<TMetadata extends OdeMsgMetadata,
    * @param metadata the metadata for this data object
    * @param payload the payload for this data object
    */
-  public OdeData(TMetadata metadata, TPayload payload) {
+  public OdeData(MetadataT metadata, PayloadT payload) {
     super();
     this.metadata = metadata;
     this.payload = payload;
